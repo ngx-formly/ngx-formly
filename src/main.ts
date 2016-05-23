8911bb1f0c1063f042127520d57bf7b94ff6d2b5
Mohammed Zama Khan <mohammedzamakhan@gmail.com>
2016-05-24 00:10:39 +0500

    feat(formly-form) remove unnessary event system to send data to formly-form

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,16 @@ import {FormlyCommon} from "./components/formly.common.component";
 import {FormlyForm} from "./components/formly.form";
 import {FormlyField} from "./components/formly.field";
 import {FormlyProviders} from "./services/formly.providers";
+import { FormlyConfigProcessor } from "./services/formly.processor";
+import {FormlyFieldVisibilityDelegate} from "./services/formly.field.delegates";
 
+export {FormlyCommon} from "./components/formly.common.component";
+export {FormlyField} from "./components/formly.field";
 export {FormlyFieldConfig} from "./components/formly.field.config";
+export {FormlyForm} from "./components/formly.form";
 export {FormlyConfig} from "./services/formly.config";
 export {FormlyPubSub, FormlyEventEmitter} from "./services/formly.event.emitter";
 export {FormlyMessage, FormlyMessages} from "./services/formly.messages";
-export {FormlyCommon} from "./components/formly.common.component";
-export {FormlyForm} from "./components/formly.form";
-export {FormlyField} from "./components/formly.field";
 export {FormlyFieldVisibilityDelegate} from "./services/formly.field.delegates"
 export {FormlyProviders} from "./services/formly.providers";
+export { FormlyConfigProcessor } from "./services/formly.processor";
