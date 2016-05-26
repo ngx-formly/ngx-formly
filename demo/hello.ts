23e323984892e8eebb02a964160a7b1bc7dcfb44
Mohammed Zama Khan <mohammedzamakhan@gmail.com>
2016-05-26 08:41:47 +0500

    chore(version) bumping the version to beta.3

--- demo/hello.ts
+++ demo/hello.ts
@@ -67,7 +67,7 @@ export class HelloApp {
     };
     this.env = {
       angularVersion: "2.0.0-rc.1",
-      formlyVersion: "2.0.0-beta.2"
+      formlyVersion: "2.0.0-beta.3"
     };
     fc.setType({
       name: "toggle",
