7ca755279dfec580c4818b0d4b04443fe00ecfb5
danielcrisp <daniel@danielcrisp.com>
2016-11-01 15:46:33 +0000

    Use the main form as the parent formGroup

--- src/ui-bootstrap/types/radio.ts
+++ src/ui-bootstrap/types/radio.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, AbstractControl } from '@angular/forms';
-import { FieldType, FormlyFieldConfig } from '../../core/core';
+import { FieldType } from '../../core/core';
 
 @Component({
   selector: 'formly-field-radio',
   template: `
-    <div [formGroup]="formControl">
+    <div [formGroup]="form">
     <div *ngFor="let option of templateOptions.options; let i = index" class="radio">
       <label class="custom-control custom-radio">
-        <input [id]="id" type="radio" [value]="option.key" [attr.name]="key" [formControlName]="i"
+        <input [id]="id" type="radio" [value]="option.key" [formControlName]="key"
         [formlyAttributes]="templateOptions" class="custom-control-input">
         {{option.value}}
         <span class="custom-control-indicator"></span>
@@ -17,14 +16,4 @@ import { FieldType, FormlyFieldConfig } from '../../core/core';
     </div>
   `,
 })
-export class FormlyFieldRadio extends FieldType {
-  static createControl(model: any, field: FormlyFieldConfig): AbstractControl {
-    let controls = [];
-
-    field.templateOptions.options.forEach((option) => {
-      controls.push(new FormControl(model));
-    });
-
-    return new FormArray(controls);
-  }
-}
+export class FormlyFieldRadio extends FieldType {}
