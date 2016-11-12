dfbb19ffb9976857f9654ce7ae3b305829fa205f
zamamoha <zamamoha@cisco.com>
2016-11-12 07:17:54 -0600

    chore(upgrade) upgrade to v2.0.0-beta.12

--- demo/hello.ts
+++ demo/hello.ts
@@ -36,7 +36,7 @@ export class HelloApp {
     };
     this.env = {
       angularVersion: '2.1.1',
-      formlyVersion: '2.0.0-beta.11',
+      formlyVersion: '2.0.0-beta.12',
     };
 
     let userFields: Array<FormlyFieldConfig> = [{
