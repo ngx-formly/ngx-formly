0629c7bd7e807300a6f273e9215f626c5cb43218
Tony Franzese <tony.franzese@woolpert.com>
2016-11-21 13:33:15 -0500

    add class properties to access template options

--- src/ui-bootstrap/types/select.ts
+++ src/ui-bootstrap/types/select.ts
@@ -5,12 +5,14 @@ import { FieldType } from '../../core/core';
   selector: 'formly-field-select',
   template: `
     <select [id]="id" [formControl]="formControl" class="form-control" [formlyAttributes]="field">
-          <option value="" *ngIf="to.placeholder">{{to.placeholder}}</option>
-          <option *ngFor="let option of to.options" [value]="option[to.valueProp || 'value']">
-              {{option[to.labelProp || 'label']}}
-          </option>
-      </select>
+      <option value="" *ngIf="to.placeholder">{{to.placeholder}}</option>
+      <option *ngFor="let option of to.options" [value]="option[valueProp]">
+        {{option[labelProp]}}
+      </option>
+    </select>
   `,
 })
 export class FormlyFieldSelect extends FieldType {
+  get labelProp(): string { return this.to['labelProp'] || 'label'; }
+  get valueProp(): string { return this.to['valueProp'] || 'value'; }
 }
