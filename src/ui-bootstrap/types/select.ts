5262f7180b10f941440dd6d6bcfb767fa628724e
Tony Franzese <franzeal@users.noreply.github.com>
2016-11-21 12:34:53 -0600

    feat(#258) valueProp and labelProp in type select (#274)
    * feat(#258) valueProp and labelProp in type select
    
    change select type to use labelProp and valueProp template options (similar to angular-formly-templates-bootstrap)
    
    * add class properties to access template options

--- src/ui-bootstrap/types/select.ts
+++ src/ui-bootstrap/types/select.ts
@@ -6,9 +6,13 @@ import { FieldType } from '../../core/core';
   template: `
     <select [id]="id" [formControl]="formControl" class="form-control" [formlyAttributes]="field">
       <option value="" *ngIf="to.placeholder">{{to.placeholder}}</option>
-      <option *ngFor="let option of to.options" [value]="option.value">{{option.label}}</option>
+      <option *ngFor="let option of to.options" [value]="option[valueProp]">
+        {{option[labelProp]}}
+      </option>
     </select>
   `,
 })
 export class FormlyFieldSelect extends FieldType {
+  get labelProp(): string { return this.to['labelProp'] || 'label'; }
+  get valueProp(): string { return this.to['valueProp'] || 'value'; }
 }
