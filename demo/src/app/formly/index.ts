3ccad917ca7c167facf0b117bd70064155e52c5c
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-11-11 07:41:20 +0000

    chore: update packages + enable IE polyfills. (#560)

--- demo/src/app/formly/index.ts
+++ demo/src/app/formly/index.ts
@@ -5,31 +5,49 @@ import { FormlyFieldToggle } from './types/toggle';
 import { FormlyWrapperHorizontalLabel } from './wrappers/horizontal.wrapper';
 import { FormlyPanelWrapper } from './wrappers/panel.wrapper';
 
+export function dateFormatValidator(control) {
+  return !control.value || control.value.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
+}
+
+export function requiredValidationMessage(err, field) {
+  return `${field.templateOptions.label} is required.`;
+}
+
+export function minlengthValidationMessage(err, field) {
+  return `Should have atleast ${err.requiredLength} characters`;
+}
+
+export function showErrorOption(field: Field) {
+  return (field.formState.submitted || field.formControl.touched || (field.field.validation && field.field.validation.show)) && !field.formControl.valid;
+}
+
 export const NgFormlyConfig: ConfigOption = {
   types: [
     { name: 'toggle', component: FormlyFieldToggle, defaultOptions: { templateOptions: { isAlert: false, isLarge: true }}},
     { name: 'horizontalInput', extends: 'input'},
     { name: 'repeatSection', component: RepeatComponent },
-    { name: 'dateFormat', defaultOptions: { templateOptions: {
-      placeholder: 'dd/mm/yyyy such as 20/05/2015',
-      dateFormat: 'DD, d  MM, yy',
-      addonLeft: {
-        class: 'fa fa-usd',
+    {
+      name: 'dateFormat',
+      defaultOptions: {
+        templateOptions: {
+          placeholder: 'dd/mm/yyyy such as 20/05/2015',
+          dateFormat: 'DD, d  MM, yy',
+          addonLeft: {
+            class: 'fa fa-usd',
+          },
+        },
+        validators: {
+          date: dateFormatValidator,
+        },
       },
     },
-    validators: {
-      date: control => !control.value || control.value.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/) },
-    },
-  }],
+  ],
   validators: [{ name: 'required', validation: Validators.required}],
   validationMessages: [
-    { name: 'required', message: (err, field) => `${field.templateOptions.label} is required.`},
+    { name: 'required', message: requiredValidationMessage },
     { name: 'invalidEmailAddress', message: 'Invalid Email Address' },
     { name: 'maxlength', message: 'Maximum Length Exceeded.' },
-    { name: 'minlength', message: (err) => {
-        return `Should have atleast ${err.requiredLength} Characters`;
-      },
-    },
+    { name: 'minlength', message: minlengthValidationMessage },
     { name: 'not_matching', message: 'Password Not Matching' },
     { name: 'zipCode', message: 'ZIP code should be 5 characters'},
     { name: 'uniqueUsername', message: 'This username is already taken.'},
@@ -39,9 +57,7 @@ export const NgFormlyConfig: ConfigOption = {
     { name: 'panel', component: FormlyPanelWrapper },
   ],
   extras: {
-    showError: (field: Field) => {
-      return (field.formState.submitted || field.formControl.touched || (field.field.validation && field.field.validation.show)) && !field.formControl.valid;
-    },
+    showError: showErrorOption,
   },
 };
 
