766bce5dc881a451df604fb0a865c4038aacaab6
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-11-19 16:00:44 +0000

    feat(demo): simplify `repeat` field type.

--- demo/src/app/formly/types/repeatedSection.ts
+++ demo/src/app/formly/types/repeatedSection.ts
@@ -9,7 +9,7 @@ import * as clonedeep from 'lodash.clonedeep';
     <div *ngFor="let control of formControl.controls; let i = index;">
       <formly-form
         [model]="model[i]"
-        [fields]="fields(i)"
+        [fields]="fields[i]"
         [options]="options"
         [form]="this.formControl.at(i)"
         [ngClass]="field.fieldArray.fieldGroupClassName">
@@ -25,7 +25,7 @@ import * as clonedeep from 'lodash.clonedeep';
 })
 export class RepeatComponent extends FieldType implements OnInit {
   formControl: FormArray;
-  _fields = [];
+  fields = [];
 
   constructor(private builder: FormlyFormBuilder) {
     super();
@@ -43,30 +43,20 @@ export class RepeatComponent extends FieldType implements OnInit {
 
   add() {
     const form = new FormGroup({}),
-      i = this._fields.length;
+      i = this.fields.length;
 
     if (!this.model[i]) {
       this.model.push({});
     }
 
-    this._fields.push(this.newFields);
-    this.builder.buildForm(form, this._fields[i], this.model[i], this.options);
+    this.fields.push(this.newFields);
+    this.builder.buildForm(form, this.fields[i], this.model[i], this.options);
     this.formControl.push(form);
   }
 
   remove(i) {
     this.formControl.removeAt(i);
     this.model.splice(i, 1);
-    this._fields.splice(i, 1);
-  }
-
-  fields(i) {
-    if (this._fields[i]) {
-      return this._fields[i];
-    }
-
-    this._fields.splice(i, 0, this.newFields);
-
-    return this._fields[i];
+    this.fields.splice(i, 1);
   }
 }
