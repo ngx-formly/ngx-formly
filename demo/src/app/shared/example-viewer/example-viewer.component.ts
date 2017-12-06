99ff9f37dbac0f5435b9e37fa593bc32fa9a289e
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-12-06 12:07:42 +0000

    feat(demo): add copy to clipboard to example-viewer.

--- demo/src/app/shared/example-viewer/example-viewer.component.ts
+++ demo/src/app/shared/example-viewer/example-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CopierService } from '../copier/copier.service';
 
 @Component({
   selector: 'formly-example-viewer',
@@ -12,7 +13,13 @@ export class ExampleViewerComponent {
   /** Whether the source for the example is being displayed. */
   showSource = false;
 
+  constructor(private copier: CopierService) {}
+
   toggleSourceView() {
     this.showSource = !this.showSource;
+  }
+
+  copySource(content) {
+    this.copier.copyText(content.innerText);
   }
 }
