328e991c63ed3443bb8b313827eccb1164ef1882
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-12-09 16:35:34 +0000

    demo: add force show error example.

--- demo/src/app/examples/examples.component.ts
+++ demo/src/app/examples/examples.component.ts
@@ -51,6 +51,7 @@ export class ExamplesComponent {
       { href: './other/disable-submit-button', text: 'Disable submit button' },
       { href: './other/advanced-layout-flex', text: 'Advanced Layout (Flex)' },
       { href: './other/matching-two-fields', text: 'Matching Two Fields' },
+      { href: './other/force-show-error', text: 'Force showing error state' },
     ]},
   ];
 }
