357cb6a6728e24d00e0c1c8c1de6b4fa10c37f7e
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-12-19 12:11:40 +0000

    fix(bootstrap): ensure to set a string value when select is not multiple.
    fix #601

--- src/ui-bootstrap/src/types/select.ts
+++ src/ui-bootstrap/src/types/select.ts
@@ -17,12 +17,11 @@ export class SelectOption {
 @Component({
   selector: 'formly-field-select',
   template: `
-    <select class="form-control"
+    <select *ngIf="to.multiple; else singleSelect" class="form-control"
       [formControl]="formControl"
       [class.is-invalid]="showError"
-      [multiple]="to.multiple"
+      [multiple]="true"
       [formlyAttributes]="field">
-      <option value="" *ngIf="!to.multiple">{{ to.placeholder }}</option>
       <ng-container *ngFor="let item of selectOptions">
        <optgroup *ngIf="item.group" label="{{item.label}}">
          <option *ngFor="let child of item.group" [value]="child.value" [disabled]="item.disabled">
@@ -32,6 +31,23 @@ export class SelectOption {
        <option *ngIf="!item.group" [value]="item.value" [disabled]="item.disabled">{{ item.label }}</option>
       </ng-container>
     </select>
+
+    <ng-template #singleSelect>
+      <select class="form-control"
+        [formControl]="formControl"
+        [class.is-invalid]="showError"
+        [formlyAttributes]="field">
+        <option value="">{{ to.placeholder }}</option>
+        <ng-container *ngFor="let item of selectOptions">
+         <optgroup *ngIf="item.group" label="{{item.label}}">
+           <option *ngFor="let child of item.group" [value]="child.value" [disabled]="item.disabled">
+             {{ child.label }}
+           </option>
+         </optgroup>
+         <option *ngIf="!item.group" [value]="item.value" [disabled]="item.disabled">{{ item.label }}</option>
+        </ng-container>
+      </select>
+    </ng-template>
   `,
 })
 export class FormlyFieldSelect extends FieldType {
