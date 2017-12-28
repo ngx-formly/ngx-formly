382587453f8a4ff1257c0832ee3dc11ace9cb111
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-12-28 20:50:45 +0000

    demo: add Nested Forms example. (#619)

---
+++ demo/src/app/examples/other/nested-formly-forms/panel-wrapper.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { FieldWrapper } from '@ngx-formly/core';
+
+@Component({
+  selector: 'formly-wrapper-panel',
+  template: `
+    <div class="card">
+      <h3 class="card-header">{{ to.label }}</h3>
+      <div class="card-body">
+        <ng-container #fieldComponent></ng-container>
+      </div>
+    </div>
+  `,
+})
+export class PanelWrapperComponent extends FieldWrapper {
+  @ViewChild('fieldComponent', {read: ViewContainerRef}) fieldComponent: ViewContainerRef;
+}
