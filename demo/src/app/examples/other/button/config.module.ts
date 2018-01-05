d5d3b9db3bae3584c90c7036977027c0f6827351
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-01-05 12:05:37 +0000

    demo: add button type example.

---
+++ demo/src/app/examples/other/button/config.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [{
+            title: 'Buttons',
+            description: `
+              This demonstrates a type for buttons. Specifically using bootstrap but this could easily be adapted for other frameworks
+            `,
+            component: AppComponent,
+            files: [
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts') },
+              { file: 'button-type.component', content: require('!!prismjs-loader?lang=typescript!./button-type.component') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts') },
+            ],
+          }],
+        },
+      },
+    ]),
+  ],
+})
+export class ConfigModule { }
