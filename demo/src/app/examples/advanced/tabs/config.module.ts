901a4313127cb8dc52c808ae3a735ee33fc0f903
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-02-16 11:07:14 +0000

    demo: add Tabs integration example. (#722)

---
+++ demo/src/app/examples/advanced/tabs/config.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [{
+            title: 'Multi-Step Form',
+            description: `
+              This is an example of Tabs form using <a href="https://material.angular.io/components/tabs/overview" target="_blank">Material Tabs</a>
+            `,
+            component: AppComponent,
+            files: [
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts') },
+            ],
+          }],
+        },
+      },
+    ]),
+  ],
+})
+export class ConfigModule { }
