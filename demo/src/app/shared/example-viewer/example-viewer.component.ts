15b4c4845c3450cfa98b8bef729976c3cf7656be
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-02-26 11:54:17 +0000

    fix(demo): take account of model changes in example-viewer. (#755)
    fix #754

--- demo/src/app/shared/example-viewer/example-viewer.component.ts
+++ demo/src/app/shared/example-viewer/example-viewer.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit, OnDestroy, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentRef, ElementRef, AfterViewInit } from '@angular/core';
 import { CopierService } from '../copier/copier.service';
-import { startWith } from 'rxjs/operators/startWith';
 import JSONFormatter from 'json-formatter-js';
 
 @Component({
@@ -8,7 +7,7 @@ import JSONFormatter from 'json-formatter-js';
   templateUrl: './example-viewer.component.html',
   styleUrls: ['./example-viewer.component.scss'],
 })
-export class ExampleViewerComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ExampleViewerComponent implements OnInit, OnDestroy {
   @Input() title;
   @Input() description;
   @Input() component;
@@ -23,26 +22,19 @@ export class ExampleViewerComponent implements OnInit, AfterViewInit, OnDestroy 
 
   constructor(private copier: CopierService, private componentFactoryResolver: ComponentFactoryResolver) {}
 
+  get model() {
+    if (this.modelPreviewRef && this.modelPreviewRef.nativeElement) {
+      const formatter = new JSONFormatter(this.demoComponentRef.instance.model, 5, { hoverPreviewEnabled: true });
+      this.modelPreviewRef.nativeElement.innerHTML = '';
+      this.modelPreviewRef.nativeElement.appendChild(formatter.render());
+    }
+
+    return this.demoComponentRef.instance.model;
+  }
+
   ngOnInit() {
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.component);
     this.demoComponentRef = this.demoRef.createComponent(componentFactory);
-  }
-
-  ngAfterViewInit() {
-    if (!this.demoComponentRef.instance.form) {
-      return;
-    }
-
-    this.demoComponentRef.instance.form.valueChanges.pipe(
-      startWith(this.demoComponentRef.instance.model),
-    )
-    .subscribe(() => {
-      if (this.modelPreviewRef && this.modelPreviewRef.nativeElement) {
-        const formatter = new JSONFormatter(this.demoComponentRef.instance.model, 5, { hoverPreviewEnabled: true });
-        this.modelPreviewRef.nativeElement.innerHTML = '';
-        this.modelPreviewRef.nativeElement.appendChild(formatter.render());
-      }
-    });
   }
 
   ngOnDestroy() {
