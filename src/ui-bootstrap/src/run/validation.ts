cb5e980eba712085c9d53e3fe2c59c50cb68f999
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-02-28 10:44:55 +0000

    fix(bootstrap): show error message when asyncValidators is set. (#759)
    fix #757

--- src/ui-bootstrap/src/run/validation.ts
+++ src/ui-bootstrap/src/run/validation.ts
@@ -3,7 +3,7 @@ import { FormlyFieldConfig, FormlyConfig } from '@ngx-formly/core';
 export class TemplateValidation {
   run(fc: FormlyConfig) {
     fc.templateManipulators.postWrapper.push((field: FormlyFieldConfig) => {
-      if (field && field.validators) {
+      if (field && (field.validators || field.asyncValidators)) {
         return 'validation-message';
       }
     });
