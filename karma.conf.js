87586a1f0a15a3fe02fe8c1c01fd2e5cc23f4fa3
Juri <juri.strumpflohner@gmail.com>
2018-03-06 09:02:07 +0100

    chore: adjust karma conf to use Chrome in watch mode

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var path = require('path');
 var webpackConfig = require('./.config/webpack.test');
 
 var ENV = process.env.npm_lifecycle_event;
-var isTestWatch = ENV === 'test-watch';
+var isTestWatch = ENV === 'test:watch';
 
 module.exports = function (config) {
   var _config = {
