69a146a59385557d5c92d0887cf25eff0018cd0f
Juri <juri.strumpflohner@gmail.com>
2018-03-06 09:59:20 +0100

    feat(core): allow binding Observables to select options
    See #693

---
+++ demo/src/app/examples/other/observable-select/config.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [{
+            title: 'Bind Observables to Select',
+            description: ``,
+            component: AppComponent,
+            files: [
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts') },
+              { file: 'data.service.ts', content: require('!!prismjs-loader?lang=typescript!./data.service.ts') },
+            ],
+          }],
+        },
+      },
+    ]),
+  ],
+})
+export class ConfigModule { }
