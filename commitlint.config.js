3d6f56ce6aa84e605a2874aafde45464f5ae5fae
Juri <juri.strumpflohner@gmail.com>
2018-04-07 12:49:10 +0200

    build: add commitlint & husky

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'subject-max-length': [1, 'always', 72],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'core',
+        'material',
+        'bootstrap',
+        'demo',
+        'guides'
+      ]
+    ],
+    'scope-empty': [1, 'never'],
+    'scope-case': [2, 'always', 'lowerCase'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test']
+    ]
+  }
+};
