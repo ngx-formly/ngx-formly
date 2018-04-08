6acca6ac893d05c68111d69f66d6b04c08d3723b
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-04-08 10:39:25 +0100

    feat(material): implement DatePicker type
    fix #614

---
+++ src/ui-material/datepicker/src/datepicker.type.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { FieldType } from '@ngx-formly/material';
+import { MatInput } from '@angular/material';
+
+@Component({
+  selector: 'formly-field-mat-datepicker',
+  template: `
+    <input matInput
+      [errorStateMatcher]="errorStateMatcher"
+      [formControl]="formControl"
+      [matDatepicker]="picker"
+      [matDatepickerFilter]="to.datepickerOptions.filter"
+      [formlyAttributes]="field">
+    <ng-template #matSuffix>
+      <mat-datepicker-toggle [for]="picker"></mat-datepicker-toggle>
+    </ng-template>
+    <mat-datepicker #picker
+      [touchUi]="to.datepickerOptions.touchUi"
+      [startView]="to.datepickerOptions.startView"
+      [startAt]="to.datepickerOptions.startAt">
+    </mat-datepicker>
+  `,
+})
+export class FormlyDatepickerTypeComponent extends FieldType {
+  @ViewChild(MatInput) formFieldControl: MatInput;
+}
