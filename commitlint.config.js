ff048630c6f02c55c6eb9ce5c76c868a59b7b432
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-04-10 10:07:56 +0100

    chore: prepare v3 publish (#879)

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,7 +19,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test']
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test', 'chore']
     ]
   }
 };
