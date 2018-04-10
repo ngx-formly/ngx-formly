93e116710d459788a70d3f1f39b209a833d2bf38
Tiger <tanhongming@hotmail.com>
2018-04-10 17:05:30 +0800

    feat(ionic): initial work for ui-ionic support (#659)

---
+++ src/ui-ionic/src/types/checkbox.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
+
+@Component({
+  selector: 'formly-field-ion-checkbox',
+  template: `
+    <ion-item>
+      <ion-label>{{ to.label }}</ion-label>
+      <ion-checkbox
+        [formControl]="formControl"
+        [formlyAttributes]="field">
+      </ion-checkbox>
+    </ion-item>
+  `,
+})
+export class FormlyFieldCheckbox extends FieldType {
+}
