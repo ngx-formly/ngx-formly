3942e0aa2a1d2a554542b24e37fbc432406f5301
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-04-21 21:45:28 +0100

    chore(release): 3.0.0-alpha.0

--- .config/publish.js
+++ .config/publish.js
@@ -5,10 +5,11 @@ const execSync = require('child_process').execSync,
     'material',
     'ionic',
     'primeng',
+    'kendo',
   ];
 
 packages.map(package => {
   const packagePath = `${__dirname}/../dist/${package}`;
 
-  execSync(`cd ${packagePath} && npm publish --tag next`);
+  execSync(`cd ${packagePath} && npm publish --tag next --access public`);
 });
