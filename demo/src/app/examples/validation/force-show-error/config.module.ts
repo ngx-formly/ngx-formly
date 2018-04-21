655b7a48bff82229cabdd6097173cca8e91fea63
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-04-21 18:10:35 +0100

    docs(demo): add StackBlitz edit button (#902)

--- demo/src/app/examples/validation/force-show-error/config.module.ts
+++ demo/src/app/examples/validation/force-show-error/config.module.ts
@@ -20,9 +20,9 @@ import { AppComponent } from './app.component';
             `,
             component: AppComponent,
             files: [
-              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html') },
-              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts') },
-              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts') },
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html'), filecontent: require('!!raw-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.component.ts') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.module.ts') },
             ],
           }],
         },
--- demo/src/app/ui/common/textarea/app.component.ts
+++ demo/src/app/ui/common/textarea/app.component.ts
@@ -3,7 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
-  selector: 'app-component',
+  selector: 'formly-app-example',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
