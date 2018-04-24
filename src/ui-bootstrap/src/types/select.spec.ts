97431032556ad723a540b2fdc631390433526550
thorgod <thorgod@users.noreply.github.com>
2018-04-24 11:25:35 -0700

    Updated Unit Test
    Updated Unit Test

--- src/ui-bootstrap/src/types/select.spec.ts
+++ src/ui-bootstrap/src/types/select.spec.ts
@@ -1,3 +1,5 @@
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { createGenericTestComponent } from '../../../core/src/test-utils';
 import { By } from '@angular/platform-browser';
@@ -10,20 +12,27 @@ import { FormlyForm } from '../../../core';
 import { of } from 'rxjs/observable/of';
 
 const createTestComponent = (html: string) =>
-    createGenericTestComponent(html, TestComponent) as ComponentFixture<TestComponent>;
+  createGenericTestComponent(html, TestComponent) as ComponentFixture<TestComponent>;
 
 let testComponentInputs;
 
-describe('ui-bootstrap: Formly Field Select Component', () => {
+describe('ui-material: Formly Field Select Component', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [TestComponent, FormlyFieldSelect], imports: [FormlyModule.forRoot({
-      types: [
-        {
-          name: 'select',
-          component: FormlyFieldSelect,
-        },
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, FormlyFieldSelect],
+      imports: [
+        NoopAnimationsModule,
+        MatSelectModule,
+        FormlyModule.forRoot({
+          types: [
+            {
+              name: 'select',
+              component: FormlyFieldSelect,
+            },
+          ],
+        }),
       ],
-    })]});
+    });
   });
 
   describe('options', () => {
@@ -37,22 +46,26 @@ describe('ui-bootstrap: Formly Field Select Component', () => {
 
 
     it('should correctly bind to a static array of data', () => {
-        testComponentInputs.fields = [{
-            key: 'sportId',
-            type: 'select',
-            templateOptions: {
-                options: [
-                    { id: '1', name: 'Soccer' },
-                    { id: '2', name: 'Basketball' },
-                ],
-                valueProp: 'id',
-                labelProp: 'name',
-            },
-        }];
+      testComponentInputs.fields = [{
+        key: 'sportId',
+        type: 'select',
+        templateOptions: {
+          options: [
+            { id: '1', name: 'Soccer' },
+            { id: '2', name: 'Basketball' },
+          ],
+          valueProp: 'id',
+          labelProp: 'name',
+        },
+      }];
 
-        const fixture = createTestComponent('<formly-form [form]="form" [fields]="fields" [model]="model" [options]="options"></formly-form>');
+      const fixture = createTestComponent('<formly-form [form]="form" [fields]="fields" [model]="model" [options]="options"></formly-form>'),
+        trigger = fixture.debugElement.query(By.css('.mat-select-trigger')).nativeElement;
 
-        expect(fixture.debugElement.query(By.css('select')).nativeElement.options.length).toEqual(2);
+      trigger.click();
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.queryAll(By.css('mat-option')).length).toEqual(2);
     });
 
     it('should correctly bind to an Observable', async(() => {
@@ -62,25 +75,29 @@ describe('ui-bootstrap: Formly Field Select Component', () => {
       ]);
 
       testComponentInputs.fields = [{
-          key: 'sportId',
-          type: 'select',
-          templateOptions: {
-              options: sports$,
-              valueProp: 'id',
-              labelProp: 'name',
-          },
+        key: 'sportId',
+        type: 'select',
+        templateOptions: {
+          options: sports$,
+          valueProp: 'id',
+          labelProp: 'name',
+        },
       }];
 
-      const fixture = createTestComponent('<formly-form [form]="form" [fields]="fields" [model]="model" [options]="options"></formly-form>');
+      const fixture = createTestComponent('<formly-form [form]="form" [fields]="fields" [model]="model" [options]="options"></formly-form>'),
+        trigger = fixture.debugElement.query(By.css('.mat-select-trigger')).nativeElement;
 
-      expect(fixture.debugElement.query(By.css('select')).nativeElement.options.length).toEqual(2);
+      trigger.click();
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.queryAll(By.css('mat-option')).length).toEqual(2);
     }));
 
   });
 
 });
 
-@Component({selector: 'formly-form-test', template: '', entryComponents: []})
+@Component({ selector: 'formly-form-test', template: '', entryComponents: [] })
 class TestComponent {
   @ViewChild(FormlyForm) formlyForm: FormlyForm;
 
