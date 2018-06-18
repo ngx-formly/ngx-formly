6a82e8b7dfdabac1ef7f18b6e5be36d5374f7439
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-06-18 22:12:26 +0100

    fix(bootstrap): allow disabling the default templateManipulator wrappers
    fix #1003

--- src/bootstrap/src/lib/run/description.ts
+++ src/bootstrap/src/lib/run/description.ts
@@ -2,6 +2,10 @@ import { FormlyFieldConfig, FormlyConfig } from '@ngx-formly/core';
 
 export class TemplateDescription {
   run(fc: FormlyConfig) {
-    fc.templateManipulators.postWrapper.push((field: FormlyFieldConfig) => 'description');
+    fc.templateManipulators.postWrapper.push((field: FormlyFieldConfig) => {
+      if (!field.templateOptions || field.templateOptions.descriptionWrapper !== false) {
+        return 'description';
+      }
+    });
   }
 }
