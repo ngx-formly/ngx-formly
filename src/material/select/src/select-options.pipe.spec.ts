601d586e97d2a49469c46e52a34a6f05020849b6
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-06-27 13:06:43 +0100

    feat(material): allow passing function for labelProp and valueProp
    fix #982

--- src/material/select/src/select-options.pipe.spec.ts
+++ src/material/select/src/select-options.pipe.spec.ts
@@ -20,29 +20,125 @@ describe('Pipe: FormlySelectOptionsPipe', () => {
     });
   });
 
-  it('group options', () => {
-    const options = [
-      { label: '1', value: '1', parent: '1' },
-      { label: '2', value: '2', parent: '1' },
-      { label: '3', value: '3', parent: '2' },
-    ];
+  describe('label & value props', () => {
+    let options;
+    beforeEach(() => {
+      options = [{ name: 'foo', id: '1' }];
+    });
 
-    pipe.transform(options, 'parent').subscribe((options) => {
-      expect(options).toEqual([
-        {
-          label: '1',
-          group: [
-            { label: '1', value: '1', parent: '1' },
-            { label: '2', value: '2', parent: '1' },
-          ],
+    it('as a string', () => {
+      const field = {
+        templateOptions: {
+          labelProp: 'name',
+          valueProp: 'id',
         },
-        {
-          label: '2',
-          group: [
-            { label: '3', value: '3', parent: '2' },
-          ],
+      };
+
+      pipe.transform(options, field).subscribe((options) => {
+        expect(options).toEqual([{ label: 'foo', value: '1' }]);
+      });
+    });
+
+    it('with group props', () => {
+      options = [
+        { name: '1', id: '1', parent: '1' },
+        { name: '2', id: '2', parent: '1' },
+        { name: '3', id: '3', parent: '2' },
+      ];
+
+      const field = {
+        templateOptions: {
+          labelProp: item => item.name,
+          valueProp: item => item.id,
+          groupProp: item => item.parent,
         },
-      ]);
+      };
+
+      pipe.transform(options, field).subscribe((options) => {
+        expect(options).toEqual([
+          {
+            label: '1',
+            group: [
+              { label: '1', value: '1' },
+              { label: '2', value: '2' },
+            ],
+          },
+          {
+            label: '2',
+            group: [
+              { label: '3', value: '3' },
+            ],
+          },
+        ]);
+      });
+    });
+
+    it('as a function', () => {
+      const field = {
+        templateOptions: {
+          labelProp: item => item.name,
+          valueProp: item => item.id,
+        },
+      };
+
+      pipe.transform(options, field).subscribe((options) => {
+        expect(options).toEqual([{ label: 'foo', value: '1' }]);
+      });
+    });
+  });
+
+  describe('group options', () => {
+    let options;
+    beforeEach(() => {
+      options = [
+        { label: '1', value: '1', parent: '1' },
+        { label: '2', value: '2', parent: '1' },
+        { label: '3', value: '3', parent: '2' },
+      ];
+    });
+
+    it('as a string', () => {
+      const field = { templateOptions: { groupProp: 'parent' } };
+
+      pipe.transform(options, field).subscribe((options) => {
+        expect(options).toEqual([
+          {
+            label: '1',
+            group: [
+              { label: '1', value: '1' },
+              { label: '2', value: '2' },
+            ],
+          },
+          {
+            label: '2',
+            group: [
+              { label: '3', value: '3' },
+            ],
+          },
+        ]);
+      });
+    });
+
+    it('as a function', () => {
+      const field = { templateOptions: { groupProp: item => item.parent } };
+
+      pipe.transform(options, field).subscribe((options) => {
+        expect(options).toEqual([
+          {
+            label: '1',
+            group: [
+              { label: '1', value: '1' },
+              { label: '2', value: '2' },
+            ],
+          },
+          {
+            label: '2',
+            group: [
+              { label: '3', value: '3' },
+            ],
+          },
+        ]);
+      });
     });
   });
 });
