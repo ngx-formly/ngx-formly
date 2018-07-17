0116642140330a9fb096e1c8e445032510b1e081
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-07-17 14:18:13 +0100

    feat(core): move formlySelectOptions pipe to core

--- src/bootstrap/src/lib/bootstrap.module.ts
+++ src/bootstrap/src/lib/bootstrap.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
+import { FormlySelectModule } from '@ngx-formly/core/select';
 import { BOOTSTRAP_FORMLY_CONFIG, FIELD_TYPE_COMPONENTS } from './bootstrap.config';
-import { FormlySelectOptionsPipe } from './select-options.pipe';
 
 @NgModule({
   declarations: [
     FIELD_TYPE_COMPONENTS,
-    FormlySelectOptionsPipe,
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormlySelectModule,
     FormlyModule.forRoot(BOOTSTRAP_FORMLY_CONFIG),
   ],
 })
