21b6e268f70fb761f1fae69d9b2084cc488deb40
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-07-17 15:09:17 +0100

    feat(core): remove lifecycle hooks in FieldType
    BREAKING CHANGE: lifecycle hooks in FieldType has been removed

--- src/core/src/lib/templates/field.type.ts
+++ src/core/src/lib/templates/field.type.ts
@@ -1,13 +1,2 @@
 import { Field } from './field';
-import { OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, OnDestroy, AfterViewChecked, SimpleChanges } from '@angular/core';
-
-export abstract class FieldType extends Field implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
-  ngOnInit() {}
-  ngOnChanges(changes: SimpleChanges) {}
-  ngDoCheck() {}
-  ngAfterContentInit() {}
-  ngAfterContentChecked() {}
-  ngAfterViewInit() {}
-  ngAfterViewChecked() {}
-  ngOnDestroy() {}
-}
+export { Field as FieldType };
