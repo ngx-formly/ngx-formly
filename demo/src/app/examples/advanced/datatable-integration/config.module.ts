897b24a9cccac5077631ad6a4aea6dedb029c55d
Enrico Oliva <enrico.oliva@uniurb.it>
2018-07-25 15:08:38 +0200

    docs(demo): add example of ngx-datatable integration

---
+++ demo/src/app/examples/advanced/datatable-integration/config.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [{
+            title: 'Datatable',
+            description: `
+              This demonstrates using
+              <code>ngx-datatable</code> inside of a custom type in order to create an input datatable.
+            `,
+            component: AppComponent,
+            files: [
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html'), filecontent: require('!!raw-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.component.ts') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.module.ts') },
+              { file: 'datatable.type.ts', content: require('!!prismjs-loader?lang=typescript!./datatable.type.ts'), filecontent: require('!!raw-loader?lang=typescript!./datatable.type.ts') },
+            ],
+          }],
+        },
+      },
+    ]),
+  ],
+  entryComponents: [AppComponent],
+})
+export class ConfigModule { }
