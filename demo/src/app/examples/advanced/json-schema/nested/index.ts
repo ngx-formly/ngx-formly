12edc49686958665d8361a63199681389b7d1b7f
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-08-03 17:01:53 +0100

    docs(demo): add component in entryComponents

--- demo/src/app/examples/advanced/json-schema/nested/index.ts
+++ demo/src/app/examples/advanced/json-schema/nested/index.ts
@@ -1,7 +1,20 @@
-import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
+import { AppModule as NestedAppModule } from './app.module';
+import { AppComponent as NestedAppComponent } from './app.component';
+
+const NestedExampleConfig = {
+  title: 'Nested',
+  component: NestedAppComponent,
+  debug: false,
+  files: [
+    { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html'), filecontent: require('!!raw-loader?lang=html!./app.component.html') },
+    { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.component.ts') },
+    { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.module.ts') },
+    { file: 'array.type.ts', content: require('!!prismjs-loader?lang=typescript!./array.type.ts'), filecontent: require('!!raw-loader?lang=typescript!./array.type.ts') },
+  ],
+};
 
 export {
-  AppComponent as NestedAppComponent,
-  AppModule as NestedAppModule,
+  NestedAppModule,
+  NestedAppComponent,
+  NestedExampleConfig,
 };
