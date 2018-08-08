12abbff796dce3d136a0ae6c547e64d3f04eea5f
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-08-08 14:43:54 +0100

    refactor(core): simplify FieldArrayType

--- src/core/src/lib/templates/field-array.type.ts
+++ src/core/src/lib/templates/field-array.type.ts
@@ -6,32 +6,21 @@ import { FormlyFormBuilder } from '../services/formly.form.builder';
 export abstract class FieldArrayType extends FieldType {
   formControl: FormArray;
 
- constructor(private builder: FormlyFormBuilder) {
+  // tslint:disable-next-line
+  constructor(builder: FormlyFormBuilder) {
     super();
   }
 
   add(i?: number, initialModel?: any) {
     i = isNullOrUndefined(i) ? this.field.fieldGroup.length : i;
-
     this.model.splice(i, 0, initialModel ? clone(initialModel) : undefined);
-    this.field.fieldGroup.splice(i, 0, { ...clone(this.field.fieldArray) });
 
-    this.field.fieldGroup.forEach((field, index) => {
-      field.key = `${index}`;
-    });
-
-    const form = new FormArray([]);
-    this.builder.buildForm(form, [this.field.fieldGroup[i]], this.model, this.options);
-    this.formControl.insert(i, form.at(0));
-
-    (<any> this.options).resetTrackModelChanges();
+    (<any> this.options).buildForm();
   }
 
   remove(i: number) {
-    this.formControl.removeAt(i);
-    this.field.fieldGroup.splice(i, 1);
-    this.field.fieldGroup.forEach((f, index) => f.key = `${index}`);
     this.model.splice(i, 1);
-    (<any> this.options).resetTrackModelChanges();
+
+    (<any> this.options).buildForm();
   }
 }
