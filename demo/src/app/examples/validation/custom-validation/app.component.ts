ecd10e689102c06137dc1565a36a808bd5166974
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-08-15 21:18:59 +0100

    docs(demo): make all custom-validator optional

--- demo/src/app/examples/validation/custom-validation/app.component.ts
+++ demo/src/app/examples/validation/custom-validation/app.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormControl, ValidationErrors } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 export function IpValidator(control: FormControl): ValidationErrors {
-  return /(\d{1,3}\.){3}\d{1,3}/.test(control.value) ? null : { 'ip': true };
+  return !control.value || /(\d{1,3}\.){3}\d{1,3}/.test(control.value) ? null : { 'ip': true };
 }
 
 @Component({
@@ -48,7 +48,7 @@ export class AppComponent {
       },
       validators: {
         ip: {
-          expression: (c) => /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
+          expression: (c) => !c.value || /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
           message: (error, field: FormlyFieldConfig) => `"${field.formControl.value}" is not a valid IP Address`,
         },
       },
