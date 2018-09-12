e2fbee4825263fa99a0f8d96a9fd1735464d93e0
Bram Borggreve <borggreve@gmail.com>
2018-09-12 07:01:53 -0500

    fix(material): make sure input type number emits the correct value (#1172)

--- src/material/input/src/input.type.ts
+++ src/material/input/src/input.type.ts
@@ -5,15 +5,29 @@ import { FieldType } from '@ngx-formly/material/form-field';
 @Component({
   selector: 'formly-field-mat-input',
   template: `
-    <input matInput
+    <input *ngIf="type !== 'number'; else numberTmp"
+      matInput
       [id]="id"
-      [type]="to.type || 'text'"
+      [type]="type || 'text'"
       [errorStateMatcher]="errorStateMatcher"
       [formControl]="formControl"
       [formlyAttributes]="field"
       [placeholder]="to.placeholder">
+    <ng-template #numberTmp>
+      <input matInput
+             [id]="id"
+             type="number"
+             [errorStateMatcher]="errorStateMatcher"
+             [formControl]="formControl"
+             [formlyAttributes]="field"
+             [placeholder]="to.placeholder">
+    </ng-template>
   `,
 })
 export class FormlyFieldInput extends FieldType implements OnInit {
   @ViewChild(MatInput) formFieldControl: MatInput;
+
+  get type() {
+    return this.to.type || 'text';
+  }
 }
