ee958c1c09498734ddfb1258898a39e54f92e2bc
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-12-06 19:17:43 +0100

    fix(core): ensure init options during buildForm (#1333)

--- src/core/src/lib/services/formly.form.builder.ts
+++ src/core/src/lib/services/formly.form.builder.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ComponentFactoryResolver } from '@angular/core';
 import { FormGroup, FormArray } from '@angular/forms';
 import { FormlyConfig } from './formly.config';
-import { FormlyFieldConfig, FormlyFormOptions, FormlyFieldConfigCache } from '../components/formly.field.config';
+import { FormlyFieldConfig, FormlyFormOptions, FormlyFieldConfigCache, FormlyValueChangeEvent, FormlyFormOptionsCache } from '../components/formly.field.config';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class FormlyFormBuilder {
-  constructor(private formlyConfig: FormlyConfig) {}
+  constructor(private formlyConfig: FormlyConfig, private componentFactoryResolver: ComponentFactoryResolver) {}
 
   buildForm(formControl: FormGroup | FormArray, fieldGroup: FormlyFieldConfig[] = [], model: any, options: FormlyFormOptions) {
+    options = options || {};
+    options.formState = options.formState || {};
+
+    if (!options.showError) {
+      options.showError = this.formlyConfig.extras.showError;
+    }
+
+    if (!options.fieldChanges) {
+      options.fieldChanges = new Subject<FormlyValueChangeEvent>();
+    }
+
+    if (!(<FormlyFormOptionsCache> options)._componentFactoryResolver) {
+      (<FormlyFormOptionsCache> options)._componentFactoryResolver = this.componentFactoryResolver;
+    }
+
     this._buildForm({ fieldGroup, model, formControl, options });
   }
 
