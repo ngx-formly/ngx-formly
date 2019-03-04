fbd7110f9276ceabdbd07b749a7438a119db8cf4
wenqi73 <1264578441@qq.com>
2019-03-04 19:50:42 +0800

    feat(ng-zorro-antd): add ui-ng-zorro-antd

---
+++ src/ng-zorro-antd/src/lib/wrappers/form-field.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FieldWrapper } from '@ngx-formly/core';
+
+@Component({
+  selector: 'formly-wrapper-ng-zorro-antd-form-field',
+  template: `
+    <nz-form-item>
+      <ng-container *ngIf="to.label && to.hideLabel !== true">
+        <nz-form-label [nzRequired]="to.required && to.hideRequiredMarker !== true" [nzFor]="id">{{
+          to.label
+        }}</nz-form-label>
+      </ng-container>
+      <nz-form-control [class.has-error]="showError">
+        <ng-container #fieldComponent></ng-container>
+        <nz-form-explain *ngIf="showError">
+          <formly-validation-message [field]="field"></formly-validation-message>
+        </nz-form-explain>
+      </nz-form-control>
+    </nz-form-item>
+  `,
+})
+export class FormlyWrapperFormField extends FieldWrapper {}
