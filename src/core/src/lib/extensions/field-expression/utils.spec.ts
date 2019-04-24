7a73ea9e0e738b78d67c80cd9013709d6f11086c
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-04-24 18:10:09 +0100

    feat(core): deprecate usage of `this.field` in expressionProperties

--- src/core/src/lib/extensions/field-expression/utils.spec.ts
+++ src/core/src/lib/extensions/field-expression/utils.spec.ts
@@ -1,4 +1,13 @@
-import { evalExpression } from './utils';
+import { evalExpression, evalStringExpression } from './utils';
+
+describe('evalStringExpression', () => {
+  it('should  the value correctly', () => {
+    const warn = spyOn(console, 'warn');
+
+    evalStringExpression('this.field.templateOptions.label', []);
+    expect(warn).toHaveBeenCalledWith(`NgxFormly: using 'this.field' in expressionProperties is deprecated since v5.1, use 'field' instead.`);
+  });
+});
 
 describe('evalExpression', () => {
   it('should evaluate the value correctly', () => {
