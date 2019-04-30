f08d787ed80928dae44689d7153e777370dcd950
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-04-30 12:13:44 +0100

    feat(bootstrap): move types into separate entry points

---
+++ src/ui/bootstrap/multicheckbox/src/multicheckbox.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlySelectModule } from '@ngx-formly/core/select';
+import { FormlyBootstrapFormFieldModule } from '@ngx-formly/bootstrap/form-field';
+
+import { FormlyFieldMultiCheckbox } from './multicheckbox.type';
+
+@NgModule({
+  declarations: [FormlyFieldMultiCheckbox],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+    FormlyBootstrapFormFieldModule,
+    FormlySelectModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'multicheckbox',
+          component: FormlyFieldMultiCheckbox,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyBootstrapMultiCheckboxModule { }
---
+++ src/ui/bootstrap/select/src/select.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlySelectModule } from '@ngx-formly/core/select';
+
+import { FormlyBootstrapFormFieldModule } from '@ngx-formly/bootstrap/form-field';
+import { FormlyFieldSelect } from './select.type';
+
+@NgModule({
+  declarations: [FormlyFieldSelect],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+    FormlyBootstrapFormFieldModule,
+    FormlySelectModule,
+    FormlyModule.forChild({
+      types: [{
+        name: 'select',
+        component: FormlyFieldSelect,
+        wrappers: ['form-field'],
+      }],
+    }),
+  ],
+})
+export class FormlyBootstrapSelectModule { }
