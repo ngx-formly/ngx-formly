a2d499cb683efc6534c315cacb80f1d913096712
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-04-30 12:13:44 +0100

    feat(bootstrap): move types into separate entry points

--- src/ui/bootstrap/src/lib/bootstrap.module.ts
+++ src/ui/bootstrap/src/lib/bootstrap.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlySelectModule } from '@ngx-formly/core/select';
-import { BOOTSTRAP_FORMLY_CONFIG, FIELD_TYPE_COMPONENTS } from './bootstrap.config';
+
+import { FormlyBootstrapFormFieldModule } from '@ngx-formly/bootstrap/form-field';
+import { FormlyBootstrapInputModule } from '@ngx-formly/bootstrap/input';
+import { FormlyBootstrapTextAreaModule } from '@ngx-formly/bootstrap/textarea';
+import { FormlyBootstrapRadioModule } from '@ngx-formly/bootstrap/radio';
+import { FormlyBootstrapCheckboxModule } from '@ngx-formly/bootstrap/checkbox';
+import { FormlyBootstrapMultiCheckboxModule } from '@ngx-formly/bootstrap/multicheckbox';
+import { FormlyBootstrapSelectModule } from '@ngx-formly/bootstrap/select';
 import { FormlyBootstrapAddonsModule } from '@ngx-formly/bootstrap/addons';
 
 @NgModule({
-  declarations: [
-    FIELD_TYPE_COMPONENTS,
-  ],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormlySelectModule,
-    FormlyModule.forChild(BOOTSTRAP_FORMLY_CONFIG),
+    FormlyBootstrapFormFieldModule,
+    FormlyBootstrapInputModule,
+    FormlyBootstrapTextAreaModule,
+    FormlyBootstrapRadioModule,
+    FormlyBootstrapCheckboxModule,
+    FormlyBootstrapMultiCheckboxModule,
+    FormlyBootstrapSelectModule,
     FormlyBootstrapAddonsModule,
   ],
 })
-export class FormlyBootstrapModule {}
+export class FormlyBootstrapModule { }
