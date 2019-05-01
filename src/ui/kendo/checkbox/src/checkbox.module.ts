a56cacd551469a0d388f414743065028bc2700d5
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:08:59 +0100

    feat(kendo): move types into separate entry points (#1569)

---
+++ src/ui/kendo/checkbox/src/checkbox.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyFormFieldModule } from '@ngx-formly/kendo/form-field';
+
+import { FormlyFieldCheckbox } from './checkbox.type';
+
+@NgModule({
+  declarations: [FormlyFieldCheckbox],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'checkbox',
+          component: FormlyFieldCheckbox,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyCheckboxModule { }
