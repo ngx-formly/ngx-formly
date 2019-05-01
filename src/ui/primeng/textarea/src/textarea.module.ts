73d277b4f143688753983879c4c82925abb666c2
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:09:11 +0100

    feat(primeng): move types into separate entry points (#1568)

---
+++ src/ui/primeng/textarea/src/textarea.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormlyFormFieldModule } from '@ngx-formly/primeng/form-field';
+import { FormlyFieldTextArea } from './textarea.type';
+
+@NgModule({
+  declarations: [FormlyFieldTextArea],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextareaModule,
+
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'textarea',
+          component: FormlyFieldTextArea,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyTextAreaModule { }
