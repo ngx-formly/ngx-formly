544d2a376b5c3c92d38d64d31ba3dd39c0ab5dd2
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:19:02 +0100

    feat(ionic): move types into separate entry points

---
+++ src/ui/ionic/input/src/input.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { FormlyFormFieldModule } from '@ngx-formly/ionic/form-field';
+
+import { FormlyFieldInput } from './input.type';
+
+@NgModule({
+  declarations: [FormlyFieldInput],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'input',
+          component: FormlyFieldInput,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyInputModule { }
---
+++ src/ui/ionic/toggle/src/toggle.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { FormlyFormFieldModule } from '@ngx-formly/ionic/form-field';
+
+import { FormlyFieldToggle } from './toggle.type';
+
+@NgModule({
+  declarations: [FormlyFieldToggle],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [{
+        name: 'toggle',
+        component: FormlyFieldToggle,
+        wrappers: ['form-field'],
+      }],
+    }),
+  ],
+})
+export class FormlyToggleModule { }
