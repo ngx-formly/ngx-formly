4046ecb9fec68f32dc3bb8176d1cd0e38655beb4
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:09:11 +0100

    feat(primeng): move types into separate entry points (#1568)

---
+++ src/ui/primeng/form-field/src/form-field.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyWrapperFormField } from './form-field.wrapper';
+
+@NgModule({
+  declarations: [FormlyWrapperFormField],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+    FormlyModule.forChild({
+      wrappers: [{
+        name: 'form-field',
+        component: FormlyWrapperFormField,
+      }],
+    }),
+  ],
+})
+export class FormlyFormFieldModule { }
