73043216356887a98b2bb2dcc99ad8c2539120d3
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 09:02:06 +0100

    feat(ionic): move types into separate entry points (#1570)

---
+++ src/ui/ionic/checkbox/src/checkbox.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { FormlyFormFieldModule } from '@ngx-formly/ionic/form-field';
+
+import { FormlyFieldCheckbox } from './checkbox.type';
+
+@NgModule({
+  declarations: [FormlyFieldCheckbox],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'checkbox',
+          component: FormlyFieldCheckbox,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyCheckboxModule { }
