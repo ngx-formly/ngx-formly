f44eacbb118f6fb3170f8cb208fa4f199a47d9f4
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 09:02:06 +0100

    feat(ionic): move types into separate entry points (#1570)

---
+++ src/ui/ionic/select/src/select.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { IonicModule } from '@ionic/angular';
+import { FormlySelectModule as FormlyCoreSelectModule } from '@ngx-formly/core/select';
+
+import { FormlyFormFieldModule } from '@ngx-formly/ionic/form-field';
+import { FormlyFieldSelect } from './select.type';
+
+@NgModule({
+  declarations: [FormlyFieldSelect],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+
+    FormlyFormFieldModule,
+    FormlyCoreSelectModule,
+    FormlyModule.forChild({
+      types: [{
+        name: 'select',
+        component: FormlyFieldSelect,
+        wrappers: ['form-field'],
+      }],
+    }),
+  ],
+})
+export class FormlySelectModule { }
