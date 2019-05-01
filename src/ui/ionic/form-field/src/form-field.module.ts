956470614a3729f73cc049fc39bf7d37f8a07ebf
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 09:02:06 +0100

    feat(ionic): move types into separate entry points (#1570)

---
+++ src/ui/ionic/form-field/src/form-field.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { FormlyWrapperFormField } from './form-field.wrapper';
+import { IonFormlyAttributes } from './formly.attributes';
+
+@NgModule({
+  declarations: [FormlyWrapperFormField, IonFormlyAttributes],
+  exports: [IonFormlyAttributes],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+
+    FormlyModule.forChild({
+      wrappers: [{
+        name: 'form-field',
+        component: FormlyWrapperFormField,
+      }],
+    }),
+  ],
+})
+export class FormlyFormFieldModule { }
