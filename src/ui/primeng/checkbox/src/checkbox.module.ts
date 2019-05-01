cd0cb9dc35a3b22b5b5ea84eaeea2beaed24fa7d
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:09:11 +0100

    feat(primeng): move types into separate entry points (#1568)

---
+++ src/ui/primeng/checkbox/src/checkbox.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormlyFormFieldModule } from '@ngx-formly/primeng/form-field';
+
+import { FormlyFieldCheckbox } from './checkbox.type';
+
+@NgModule({
+  declarations: [FormlyFieldCheckbox],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    FormlyFormFieldModule,
+    FormlyModule.forChild({
+      types: [
+        {
+          name: 'checkbox',
+          component: FormlyFieldCheckbox,
+          wrappers: ['form-field'],
+        },
+      ],
+    }),
+  ],
+})
+export class FormlyCheckboxModule { }
