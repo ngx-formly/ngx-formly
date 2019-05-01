29cefc19f31193dbe24e7c344e622351bee41310
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-05-01 08:09:11 +0100

    feat(primeng): move types into separate entry points (#1568)

---
+++ src/ui/primeng/radio/src/radio.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlySelectModule } from '@ngx-formly/core/select';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormlyFormFieldModule } from '@ngx-formly/primeng/form-field';
+import { FormlyFieldRadio } from './radio.type';
+
+@NgModule({
+  declarations: [FormlyFieldRadio],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RadioButtonModule,
+
+    FormlyFormFieldModule,
+    FormlySelectModule,
+    FormlyModule.forChild({
+      types: [{
+        name: 'radio',
+        component: FormlyFieldRadio,
+        wrappers: ['form-field'],
+      }],
+    }),
+  ],
+})
+export class FormlyRadioModule { }
