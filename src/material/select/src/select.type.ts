e8f4979759970c2e60d4887ed5ce95eac2326386
Juri <juri.strumpflohner@r3-gis.com>
2019-06-03 17:14:37 +0200

    fix(material): multi-select when passing observable data

--- src/material/select/src/select.type.ts
+++ src/material/select/src/select.type.ts
@@ -1,18 +1,19 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSelect, MatSelectChange } from '@angular/material/select';
+import { Component } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { FieldType } from '@ngx-formly/material/form-field';
 
 @Component({
   selector: 'formly-field-mat-select',
   template: `
-    <ng-template #selectAll>
-      <mat-option (click)="toggleSelectAll()">
+    <ng-template #selectAll let-selectOptions="selectOptions">
+      <mat-option (click)="toggleSelectAll(selectOptions)">
         <mat-pseudo-checkbox class="mat-option-pseudo-checkbox"
-          [state]="state">
+          [state]="getState(selectOptions)">
         </mat-pseudo-checkbox>
         {{ to.selectAllOption }}
       </mat-option>
     </ng-template>
+
 
     <mat-select [id]="id"
       [formControl]="formControl"
@@ -26,29 +27,30 @@ import { FieldType } from '@ngx-formly/material/form-field';
       [aria-labelledby]="formField?._labelId"
       [disableOptionCentering]="to.disableOptionCentering"
       >
-      <ng-container *ngIf="to.multiple && to.selectAllOption" [ngTemplateOutlet]="selectAll"></ng-container>
-      <ng-container *ngFor="let item of to.options | formlySelectOptions:field | async">
-        <mat-optgroup *ngIf="item.group" [label]="item.label">
-          <mat-option *ngFor="let child of item.group" [value]="child.value" [disabled]="child.disabled">
-            {{ child.label }}
-          </mat-option>
-        </mat-optgroup>
-        <mat-option *ngIf="!item.group" [value]="item.value" [disabled]="item.disabled">{{ item.label }}</mat-option>
+      <ng-container *ngIf="to.options | formlySelectOptions:field | async as selectOptions">
+        <ng-container *ngIf="to.multiple && to.selectAllOption" [ngTemplateOutlet]="selectAll" [ngTemplateOutletContext]="{ selectOptions: selectOptions }">
+        </ng-container>
+        <ng-container *ngFor="let item of selectOptions">
+          <mat-optgroup *ngIf="item.group" [label]="item.label">
+            <mat-option *ngFor="let child of item.group" [value]="child.value" [disabled]="child.disabled">
+              {{ child.label }}
+            </mat-option>
+          </mat-optgroup>
+          <mat-option *ngIf="!item.group" [value]="item.value" [disabled]="item.disabled">{{ item.label }}</mat-option>
+        </ng-container>
       </ng-container>
     </mat-select>
   `,
 })
 export class FormlyFieldSelect extends FieldType {
-  @ViewChild(MatSelect) formFieldControl!: MatSelect;
-
   defaultOptions = {
     templateOptions: { options: [] },
   };
 
   get value() { return this.formControl.value || []; }
-  get state() {
+  getState(options: any[]) {
     if (this.value.length > 0) {
-      return this.value.length !== this.formFieldControl.options.length
+      return this.value.length !== options.length
         ? 'indeterminate'
         : 'checked';
     }
@@ -56,10 +58,10 @@ export class FormlyFieldSelect extends FieldType {
     return '';
   }
 
-  toggleSelectAll() {
+  toggleSelectAll(options: any[]) {
     this.formControl.setValue(
-      this.value.length !== this.formFieldControl.options.length
-        ? this.formFieldControl.options.map(x => x.value)
+      this.value.length !== options.length
+        ? options.map(x => x.value)
         : [],
     );
   }
