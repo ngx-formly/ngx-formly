f474c3f950a7ab79eeff1c8ae24a6bdb7538eb97
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-07-14 00:05:21 +0100

    feat(core): remove deprecated methods and configs (#1665)

--- src/core/src/lib/extensions/field-form/field-form.ts
+++ src/core/src/lib/extensions/field-form/field-form.ts
@@ -46,10 +46,6 @@ export class FieldFormExtension implements FormlyExtension {
         }
         control.updateValueAndValidity();
       }
-    } else if (field._componentFactory && field._componentFactory.component && field._componentFactory.component.createControl) {
-      const component = field._componentFactory.component;
-      console.warn(`NgxFormly: '${component.name}::createControl' is deprecated since v5.0, use 'prePopulate' hook instead.`);
-      control = component.createControl(value, field);
     } else if (field.fieldGroup) {
       // TODO: move to postPopulate
       control = new FormGroup({}, controlOptions);
