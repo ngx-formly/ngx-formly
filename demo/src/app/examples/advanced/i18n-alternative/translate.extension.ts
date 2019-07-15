e978c7bdc9e41d944017527891d2ff9d49c9471f
Johannes Millan <johannes.millan@gmail.com>
2019-07-15 14:30:59 +0200

    docs: add alternative approach for ngx-translate

---
+++ demo/src/app/examples/advanced/i18n-alternative/translate.extension.ts
@@ -0,0 +1,27 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { TranslateService } from '@ngx-translate/core';
+
+export class TranslateExtension {
+  constructor(private translate: TranslateService) {}
+  prePopulate(field: FormlyFieldConfig) {
+    const to = field.templateOptions || {};
+    if (!to.translate || to._translated) {
+      return;
+    }
+
+    to._translated = true;
+    field.expressionProperties = {
+      ...(field.expressionProperties || {}),
+      'templateOptions.label': this.translate.stream(to.label),
+    };
+  }
+}
+
+export function registerTranslateExtension(translate: TranslateService) {
+  return {
+    extensions: [{
+      name: 'translate',
+      extension: new TranslateExtension(translate),
+    }],
+  };
+}
