c2794ab16ad193ec9e23e2c7d4aa8fd0d19eaecc
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-09-24 13:44:17 +0100

    refactor(json-schema): add resolveSchema (#1794)

--- demo/src/app/examples/advanced/json-schema/object.type.ts
+++ demo/src/app/examples/advanced/json-schema/object.type.ts
@@ -4,17 +4,18 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-object-type',
   template: `
-    <legend *ngIf="to.label">{{ to.label }}</legend>
-    <p *ngIf="to.description">{{ to.description }}</p>
-    <div class="alert alert-danger" role="alert" *ngIf="showError && formControl.errors">
-      <formly-validation-message [field]="field"></formly-validation-message>
+    <div class="mb-3">
+      <legend *ngIf="to.label">{{ to.label }}</legend>
+      <p *ngIf="to.description">{{ to.description }}</p>
+      <div class="alert alert-danger" role="alert" *ngIf="showError && formControl.errors">
+        <formly-validation-message [field]="field"></formly-validation-message>
+      </div>
+      <formly-field *ngFor="let f of field.fieldGroup" [field]="f"></formly-field>
     </div>
-
-    <formly-field *ngFor="let f of field.fieldGroup" [field]="f"></formly-field>
   `,
 })
 export class ObjectTypeComponent extends FieldType {
-    defaultOptions = {
-        defaultValue: {},
-    };
+  defaultOptions = {
+    defaultValue: {},
+  };
 }
