e7727592e27d1756e0471212cc801297a3492d5d
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-13 13:47:21 +0100

    refactor: improve build script (#1834)

--- build/deploy.ts
+++ build/deploy.ts
@@ -1,16 +1,15 @@
-const ghpages = require('gh-pages');
-const { copyFileSync } = require('fs');
-const { copySync } = require('fs-extra');
-const { join } = require('path');
-const { execSync } = require('child_process');
+import { exec } from './util';
+import { publish } from 'gh-pages';
+import { copyFileSync } from 'fs';
+import { copySync } from 'fs-extra';
+import { join } from 'path';
+
 const basePath = join(process.cwd(), 'dist/app');
-const branch = process.env.TRAVIS_BRANCH || execSync(`git branch | sed -n '/\* /s///p'`).toString().trim();
+const branch = process.env.TRAVIS_BRANCH || exec(`git branch | sed -n '/\* /s///p'`, {}).toString().trim();
 const isStableVersion = branch === 'v5';
+
 // build demo
-execSync(
-  `rm -rf dist/app && ng build --prod --deploy-url=/ngx-formly/${branch}/ --base-href=/ngx-formly/${isStableVersion ? '': branch} --output-path=dist/app/${branch}`,
-  {stdio: 'inherit'}
-);
+exec(`rm -rf dist/app && ng build --prod --deploy-url=/ngx-formly/${branch}/ --base-href=/ngx-formly/${isStableVersion ? '' : branch} --output-path=dist/app/${branch}`);
 
 copyFileSync(join(basePath, branch, 'index.html'), join(basePath, branch, '404.html'));
 if (isStableVersion) {
@@ -19,10 +18,10 @@ if (isStableVersion) {
   ['404', 'index'].forEach(page => copyFileSync(join(basePath, branch, 'index.html'), join(basePath, `${page}.html`)));
 }
 
-ghpages.publish(
-  basePath, 
+publish(
+  basePath,
   {
     only: branch,
     repo: 'https://' + process.env.GH_TOKEN + '@github.com/ngx-formly/ngx-formly.git',
-  }
+  },
 );
