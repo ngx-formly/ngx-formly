dc77714eaa43f49d2113a86b864b59296e592fb1
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-13 13:47:21 +0100

    refactor: improve build script (#1834)

---
+++ build/publish.ts
@@ -0,0 +1,7 @@
+import { PACKAGES, exec } from './util';
+
+PACKAGES.forEach(name => {
+  const pkgPath = `${__dirname}/../dist/@ngx-formly/${name}`;
+
+  exec(`cd ${pkgPath} && npm publish --tag next --access public`);
+});
