dc7404e1b173c449283786bd30364e321ee87def
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-13 13:47:21 +0100

    refactor: improve build script (#1834)

---
+++ build/util.ts
@@ -0,0 +1,16 @@
+import { execSync, ExecSyncOptions } from 'child_process';
+
+export const PACKAGES = [
+  'core',
+  'bootstrap',
+  'material',
+  'ionic',
+  'primeng',
+  'kendo',
+  'schematics',
+  'nativescript',
+];
+
+export function exec(cmd: string, options: ExecSyncOptions = {stdio: 'inherit'}) {
+  return execSync(cmd, options);
+}
