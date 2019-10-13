d7b15e41f82928798ad83cc9dc4b0bdfa7489e5f
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-13 13:47:21 +0100

    refactor: improve build script (#1834)

---
+++ build/build.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import { PACKAGES, exec } from './util';
+
+const mainPkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const distDir = 'dist/@ngx-formly';
+
+// cleanup
+exec(`rm -rf ${distDir}`);
+
+PACKAGES.map(name => {
+  // build package
+  exec(
+    name === 'schematics'
+    ? `ts-node build/schematics.ts`
+    : `ng build @ngx-formly/${name}`,
+  );
+
+  const pkgDir = `${distDir}/${name}`;
+  exec(`cp README.md ${pkgDir}`);
+
+  // update `FORMLY-VERSION` in package.json for all sub-packages
+  const pkgPath = `${pkgDir}/package.json`;
+  const pkgJson = {
+    ...JSON.parse(fs.readFileSync(pkgPath, 'utf8')),
+    version: mainPkg.version,
+    homepage: mainPkg.homepage,
+    description: mainPkg.description,
+    repository: mainPkg.repository,
+    bugs: mainPkg.bugs,
+  };
+
+  fs.writeFileSync(pkgPath, JSON.stringify(pkgJson, null, 2));
+});
