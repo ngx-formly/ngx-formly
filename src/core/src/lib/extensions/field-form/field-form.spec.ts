5b82d3bd6c5823c0300eb222b5ed2bd68b36e617
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-16 22:21:11 +0100

    feat(core): allow passing observable for expression asyncValidators (#1849)

--- src/core/src/lib/extensions/field-form/field-form.spec.ts
+++ src/core/src/lib/extensions/field-form/field-form.spec.ts
@@ -1,12 +1,19 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FieldFormExtension } from './field-form';
-import { FormlyFieldConfig } from '../../components/formly.field.config';
+import { FormlyFieldConfig, FormlyFieldConfigCache } from '../../components/formly.field.config';
 
-function createField(field: FormlyFieldConfig): FormlyFieldConfig {
-  const f = { modelOptions: {}, templateOptions: {}, ...field };
-  f.parent = { formControl: new FormGroup({}), fieldGroup: [f] };
+function buildField(field: FormlyFieldConfig): FormlyFieldConfigCache {
+  field = {
+    modelOptions: {},
+    templateOptions: {},
+    parent: { formControl: new FormGroup({}) },
+    ...field,
+  };
 
-  return f;
+  const extension = new FieldFormExtension();
+  extension.onPopulate(field);
+
+  return field;
 }
 
 describe('FieldFormExtension', () => {
@@ -17,36 +24,32 @@ describe('FieldFormExtension', () => {
 
   describe('field', () => {
     it('should create formControl when key exist', () => {
-      const field = createField({ key: 'title' });
+      const field = buildField({ key: 'title' });
 
-      extension.onPopulate(field);
       expect(field.formControl instanceof FormControl).toBeTruthy();
     });
 
     xit('should not create formControl when key is empty', () => {
-      const field = createField({});
+      const field = buildField({});
 
-      extension.onPopulate(field);
       expect(field.formControl).toBeUndefined();
     });
   });
 
   describe('fieldGroup', () => {
     it('should assign parent formcontrol when key is empty', () => {
-      const field = createField({ fieldGroup: [] });
+      const field = buildField({ fieldGroup: [] });
 
-      extension.onPopulate(field);
       expect(field.formControl).toEqual(field.parent.formControl);
     });
   });
 
   it('should override existing formcontrol when key is empty', () => {
-    const field = createField({
+    const field = buildField({
       fieldGroup: [],
       formControl: new FormControl(),
     });
 
-    extension.onPopulate(field);
     expect(field.formControl).toEqual(field.parent.formControl);
   });
 
