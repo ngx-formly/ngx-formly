4d22436e058042d2119a263bb89b535bcb3e6ffd
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-10-20 15:46:04 +0100

    test(core): rework form builder & extensions tests (#1853)

--- src/core/src/lib/extensions/core/core.ts
+++ src/core/src/lib/extensions/core/core.ts
@@ -1,13 +1,25 @@
+import { ChangeDetectorRef } from '@angular/core';
 import { FormlyExtension, FormlyConfig, TemplateManipulators } from '../../services/formly.config';
-import { FormlyFieldConfigCache, FormlyFieldConfig } from '../../components/formly.field.config';
-import { getFieldId, isUndefined, getFieldValue, reverseDeepMerge, assignFieldValue } from '../../utils';
+import { FormlyFieldConfigCache, FormlyFieldConfig, FormlyValueChangeEvent } from '../../components/formly.field.config';
+import { getFieldId, assignFieldValue, isUndefined, getFieldValue, reverseDeepMerge, defineHiddenProp, clone, isNullOrUndefined } from '../../utils';
+import { Subject } from 'rxjs';
 
 /** @experimental */
 export class CoreExtension implements FormlyExtension {
   private formId = 0;
-  constructor(private formlyConfig: FormlyConfig) { }
+  constructor(private config: FormlyConfig) { }
 
   prePopulate(field: FormlyFieldConfigCache) {
+    const root = field.parent;
+    this.initRootOptions(field);
+    if (root) {
+      Object.defineProperty(field, 'options', { get: () => root.options, configurable: true });
+      Object.defineProperty(field, 'model', {
+        get: () => field.key && field.fieldGroup ? getFieldValue(field) : root.model,
+        configurable: true,
+      });
+    }
+
     this.getFieldComponentInstance(field).prePopulate();
   }
 
@@ -27,19 +39,69 @@ export class CoreExtension implements FormlyExtension {
     this.getFieldComponentInstance(field).postPopulate();
   }
 
-  private initFieldOptions(field: FormlyFieldConfigCache) {
-    const root = <FormlyFieldConfigCache> field.parent;
-    if (!root) {
+  private initRootOptions(field: FormlyFieldConfigCache) {
+    if (field.parent) {
       return;
     }
 
-    Object.defineProperty(field, 'form', { get: () => root.formControl, configurable: true });
-    Object.defineProperty(field, 'options', { get: () => root.options, configurable: true });
-    Object.defineProperty(field, 'model', {
-      get: () => field.key && field.fieldGroup ? getFieldValue(field) : root.model,
-      configurable: true,
-    });
+    const options = field.options;
+    field.options.formState = field.options.formState || {};
+    if (!options.showError) {
+      options.showError = this.config.extras.showError;
+    }
 
+    if (!options.updateInitialValue) {
+      options.updateInitialValue = () => options._initialModel = clone(field.model);
+    }
+
+    if (!options.fieldChanges) {
+      defineHiddenProp(options, 'fieldChanges', new Subject<FormlyValueChangeEvent>());
+    }
+
+    if (!options._hiddenFieldsForCheck) {
+      options._hiddenFieldsForCheck = [];
+    }
+
+    if (!options._markForCheck) {
+      options._markForCheck = (field) => {
+        if (field._componentRefs) {
+          field._componentRefs.forEach(ref => {
+            // NOTE: we cannot use ref.changeDetectorRef, see https://github.com/ngx-formly/ngx-formly/issues/2191
+            const changeDetectorRef = ref.injector.get(ChangeDetectorRef);
+            changeDetectorRef.markForCheck();
+          });
+        }
+
+        if (field.fieldGroup) {
+          field.fieldGroup.forEach(f => options._markForCheck(f));
+        }
+      };
+    }
+
+    if (!options.resetModel) {
+      options.resetModel = (model ?: any) => {
+        model = clone(isNullOrUndefined(model) ? options._initialModel : model);
+        if (field.model) {
+          Object.keys(field.model).forEach(k => delete field.model[k]);
+          Object.assign(field.model, model || {});
+        }
+
+        options._buildField(field);
+
+        // we should call `NgForm::resetForm` to ensure changing `submitted` state after resetting form
+        // but only when the current component is a root one.
+        if (options.parentForm && options.parentForm.control === field.formControl) {
+          options.parentForm.resetForm(model);
+        } else {
+          field.formControl.reset(model);
+        }
+      };
+    }
+
+    field.options.updateInitialValue();
+  }
+
+  private initFieldOptions(field: FormlyFieldConfigCache) {
     reverseDeepMerge(field, {
       id: getFieldId(`formly_${this.formId}`, field, field['index']),
       hooks: {},
@@ -66,12 +128,12 @@ export class CoreExtension implements FormlyExtension {
       field.type = 'formly-template';
     }
 
-    if (!field.type && field.fieldGroup) {
+    if (field.parent && !field.type && field.fieldGroup) {
       field.type = 'formly-group';
     }
 
     if (field.type) {
-      this.formlyConfig.getMergedField(field);
+      this.config.getMergedField(field);
     }
 
     if (field.parent) {
@@ -104,16 +166,16 @@ export class CoreExtension implements FormlyExtension {
     };
 
     field.wrappers = [
-      ...this.formlyConfig.templateManipulators.preWrapper.map(m => m(field)),
+      ...this.config.templateManipulators.preWrapper.map(m => m(field)),
       ...fieldTemplateManipulators.preWrapper.map(m => m(field)),
       ...field.wrappers,
-      ...this.formlyConfig.templateManipulators.postWrapper.map(m => m(field)),
+      ...this.config.templateManipulators.postWrapper.map(m => m(field)),
       ...fieldTemplateManipulators.postWrapper.map(m => m(field)),
     ].filter((el, i, a) => el && i === a.indexOf(el));
   }
 
   private getFieldComponentInstance(field: FormlyFieldConfigCache) {
-    const componentRef = this.formlyConfig.resolveFieldTypeRef(field);
+    const componentRef = this.config.resolveFieldTypeRef(field);
     const instance: FormlyExtension = componentRef ? componentRef.instance as any : {};
 
     return {
