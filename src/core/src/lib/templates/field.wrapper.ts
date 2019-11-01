9a63c3eb8abd967b4977c666eeb7f93a41278ad8
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-11-01 13:26:48 +0100

    fix(core): set static flag to false for ivy support (#1893)

--- src/core/src/lib/templates/field.wrapper.ts
+++ src/core/src/lib/templates/field.wrapper.ts
@@ -4,5 +4,5 @@ import { FormlyFieldConfig } from '../components/formly.field.config';
 
 export abstract class FieldWrapper<F extends FormlyFieldConfig = FormlyFieldConfig> extends FieldType<F> {
   // TODO: remove `any`, once dropping angular `V7` support.
-  @ViewChild('fieldComponent', <any>{ read: ViewContainerRef, static: true }) fieldComponent: ViewContainerRef;
+  @ViewChild('fieldComponent', <any>{ read: ViewContainerRef, static: false }) fieldComponent: ViewContainerRef;
 }
