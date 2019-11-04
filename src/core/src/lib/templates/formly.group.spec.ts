65faf66484161d92cf07566007cae3eab3350744
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-11-04 20:44:48 +0100

    chore(core): rework formly components tests (#1897)

---
+++ src/core/src/lib/templates/formly.group.spec.ts
@@ -0,0 +1,41 @@
+import { createFormlyFieldComponent as renderComponent } from '@ngx-formly/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
+
+function getFormlyGroupField(fixture: ComponentFixture<any>): HTMLInputElement {
+  return fixture.nativeElement.querySelector('formly-group');
+}
+
+function getFormlyGroupFields(fixture: ComponentFixture<any>): HTMLInputElement[] {
+  return fixture.nativeElement.querySelectorAll('formly-group > formly-field');
+}
+
+describe('Group Field Type', () => {
+  it('should render fieldGroup', () => {
+    const fixture = renderComponent({
+      type: 'formly-group',
+      fieldGroup: [{ key: 'title1' }, { key: 'title2' }],
+    });
+
+    expect(getFormlyGroupField(fixture)).toBeDefined();
+    expect(getFormlyGroupFields(fixture).length).toEqual(2);
+  });
+
+  it('should add defaultOptions', () => {
+    const fixture = renderComponent({
+      key: 'foo',
+      type: 'formly-group',
+    });
+
+    const model = fixture.componentInstance.field.parent.model;
+    expect(model).toEqual({ foo: {} });
+  });
+
+  it('should add fieldGroupClassName', () => {
+    const fixture = renderComponent({
+      fieldGroupClassName: 'foo-class',
+      type: 'formly-group',
+    });
+
+    expect(getFormlyGroupField(fixture).getAttribute('class')).toEqual('foo-class');
+  });
+});
