c63ca7513d876772ac9adbc01db71debea552d8f
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-11-20 20:34:14 +0100

    chore(ng-zorro-antd): add test

---
+++ src/ui/ng-zorro-antd/radio/src/radio.type.spec.ts
@@ -0,0 +1,63 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { createFormlyFieldComponent } from '@ngx-formly/core/testing';
+import { FormlyNzRadioModule } from '@ngx-formly/ng-zorro-antd/radio';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const renderComponent = (field: FormlyFieldConfig) => {
+  return createFormlyFieldComponent(field, {
+    imports: [NoopAnimationsModule, FormlyNzRadioModule],
+  });
+};
+
+describe('ui-ng-zorro-antd: Radio Type', () => {
+  it('should render radio type', () => {
+    const { query, queryAll } = renderComponent({
+      key: 'name',
+      type: 'radio',
+      templateOptions: {
+        options: [
+          { value: 1, label: 'label 1' },
+          { value: 2, label: 'label 2' },
+          { value: 3, label: 'label 3' },
+        ],
+      },
+    });
+
+    expect(query('formly-wrapper-nz-form-field')).not.toBeNull();
+    expect(queryAll('label[nz-radio]')).toHaveLength(3);
+    expect(queryAll('input[type="radio"]')).toHaveLength(3);
+  });
+
+  it('should add "ng-invalid" class on invalid', () => {
+    const { query } = renderComponent({
+      key: 'name',
+      type: 'radio',
+      validation: { show: true },
+      templateOptions: {
+        options: [{ value: 1, label: 'label 1' }],
+        required: true,
+      },
+    });
+
+    expect(query('nz-radio-group').classes['ng-invalid']).toBeTrue();
+  });
+
+  it('should bind control value on change', () => {
+    const { query, field, detectChanges } = renderComponent({
+      key: 'name',
+      type: 'radio',
+      templateOptions: {
+        options: [{ value: 1, label: 'label 1' }],
+      },
+    });
+
+    const evt = {
+      stopPropagation: () => {},
+      preventDefault: () => {},
+    };
+
+    query('label[nz-radio]').triggerEventHandler('click', evt);
+    detectChanges();
+    expect(field.formControl.value).toEqual(1);
+  });
+});
