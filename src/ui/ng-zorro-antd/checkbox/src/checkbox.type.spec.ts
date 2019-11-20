bd78f2c47dea4069bfc81c6f96eaa0a4890d1844
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-11-20 20:34:14 +0100

    chore(ng-zorro-antd): add test

---
+++ src/ui/ng-zorro-antd/checkbox/src/checkbox.type.spec.ts
@@ -0,0 +1,45 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { createFormlyFieldComponent } from '@ngx-formly/core/testing';
+import { FormlyNzCheckboxModule } from '@ngx-formly/ng-zorro-antd/checkbox';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const renderComponent = (field: FormlyFieldConfig) => {
+  return createFormlyFieldComponent(field, {
+    imports: [NoopAnimationsModule, FormlyNzCheckboxModule],
+  });
+};
+
+describe('ui-ng-zorro-antd: Checkbox Type', () => {
+  it('should render checkbox type', () => {
+    const { query } = renderComponent({
+      key: 'name',
+      type: 'checkbox',
+    });
+
+    expect(query('formly-wrapper-nz-form-field')).not.toBeNull();
+
+    expect(query('.ant-checkbox-indeterminate')).not.toBeNull();
+    expect(query('input[type="checkbox"]')).not.toBeNull();
+    const { attributes } = query('label[nz-checkbox]');
+    expect(attributes).toMatchObject({
+      id: 'formly_1_checkbox_name_0',
+    });
+  });
+
+  it('should bind control value on change', () => {
+    const { query, field, detectChanges } = renderComponent({
+      key: 'name',
+      type: 'checkbox',
+    });
+
+    const inputDebugEl = query<HTMLInputElement>('input[type="checkbox"]');
+
+    inputDebugEl.triggerEventHandler('change', { target: { checked: true } });
+    detectChanges();
+    expect(field.formControl.value).toBeTrue();
+
+    inputDebugEl.triggerEventHandler('change', { target: { checked: false } });
+    detectChanges();
+    expect(field.formControl.value).toBeFalse();
+  });
+});
---
+++ src/ui/ng-zorro-antd/select/src/select.type.spec.ts
@@ -0,0 +1,50 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormlyNzSelectModule } from '@ngx-formly/ng-zorro-antd/select';
+import { createFormlyFieldComponent } from '@ngx-formly/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const renderComponent = (field: FormlyFieldConfig) => {
+  return createFormlyFieldComponent(field, {
+    imports: [NoopAnimationsModule, FormlyNzSelectModule],
+  });
+};
+
+describe('ui-ng-zorro-antd: Select Type', () => {
+  it('should render select type', () => {
+    const { query, queryAll, detectChanges } = renderComponent({
+      key: 'name',
+      type: 'select',
+      templateOptions: {
+        options: [
+          { value: 1, label: 'label 1' },
+          { value: 2, label: 'label 2' },
+          { value: 3, label: 'label 3' },
+        ],
+      },
+    });
+
+    expect(query('formly-wrapper-nz-form-field')).not.toBeNull();
+
+    query('nz-select').triggerEventHandler('click', {});
+    detectChanges();
+
+    expect(queryAll('li[nz-option-li]')).toHaveLength(3);
+  });
+
+  it('should bind control value on change', () => {
+    const { query, field, detectChanges } = renderComponent({
+      key: 'name',
+      type: 'select',
+      templateOptions: {
+        options: [{ value: 1, label: 'label 1' }],
+      },
+    });
+
+    query('nz-select').triggerEventHandler('click', {});
+    detectChanges();
+
+    query('li[nz-option-li]').triggerEventHandler('click', {});
+    detectChanges();
+    expect(field.formControl.value).toEqual(1);
+  });
+});
