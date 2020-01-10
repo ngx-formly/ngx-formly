cd198f3d8372477cdd1fe36242fb326f59c9d217
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-01-10 20:34:49 +0100

    perf(bootstrap): improve render time of long list for select ty… (#2023)
    fix #2022

--- src/bootstrap/src/lib/types/select.ts
+++ src/bootstrap/src/lib/types/select.ts
@@ -11,14 +11,26 @@ import { FieldType } from '@ngx-formly/core';
       [compareWith]="to.compareWith || compareWith"
       [class.is-invalid]="showError"
       [formlyAttributes]="field">
-        <ng-container *ngFor="let item of to.options | formlySelectOptions:field | async">
-         <optgroup *ngIf="item.group" label="{{item.label}}">
-            <option *ngFor="let child of item.group" [ngValue]="child.value" [disabled]="child.disabled">
-              {{ child.label }}
-            </option>
-          </optgroup>
-          <option *ngIf="!item.group" [ngValue]="item.value" [disabled]="item.disabled">{{ item.label }}</option>
+      <ng-container *ngIf="to.options | formlySelectOptions:field | async as opts">
+        <ng-container *ngIf="to._flatOptions else grouplist">
+          <ng-container *ngFor="let opt of opts">
+            <option [ngValue]="opt.value" [disabled]="opt.disabled">{{ opt.label }}</option>
+          </ng-container>
         </ng-container>
+
+        <ng-template #grouplist>
+          <ng-container *ngFor="let opt of opts">
+            <option *ngIf="!opt.group else optgroup" [ngValue]="opt.value" [disabled]="opt.disabled">{{ opt.label }}</option>
+            <ng-template #optgroup>
+              <optgroup [label]="opt.label">
+                <option *ngFor="let child of opt.group" [ngValue]="child.value" [disabled]="child.disabled">
+                  {{ child.label }}
+                </option>
+              </optgroup>
+            </ng-template>
+          </ng-container>
+        </ng-template>
+      </ng-container>
     </select>
 
     <ng-template #singleSelect>
@@ -30,13 +42,25 @@ import { FieldType } from '@ngx-formly/core';
         [class.is-invalid]="showError"
         [formlyAttributes]="field">
         <option *ngIf="to.placeholder" [ngValue]="null">{{ to.placeholder }}</option>
-        <ng-container *ngFor="let item of to.options | formlySelectOptions:field | async">
-          <optgroup *ngIf="item.group" label="{{item.label}}">
-            <option *ngFor="let child of item.group" [ngValue]="child.value" [disabled]="child.disabled">
-              {{ child.label }}
-            </option>
-          </optgroup>
-          <option *ngIf="!item.group" [ngValue]="item.value" [disabled]="item.disabled">{{ item.label }}</option>
+        <ng-container *ngIf="to.options | formlySelectOptions:field | async as opts">
+          <ng-container *ngIf="to._flatOptions else grouplist">
+            <ng-container *ngFor="let opt of opts">
+              <option [ngValue]="opt.value" [disabled]="opt.disabled">{{ opt.label }}</option>
+            </ng-container>
+          </ng-container>
+
+          <ng-template #grouplist>
+            <ng-container *ngFor="let opt of opts">
+              <option *ngIf="!opt.group else optgroup" [ngValue]="opt.value" [disabled]="opt.disabled">{{ opt.label }}</option>
+              <ng-template #optgroup>
+                <optgroup [label]="opt.label">
+                  <option *ngFor="let child of opt.group" [ngValue]="child.value" [disabled]="child.disabled">
+                    {{ child.label }}
+                  </option>
+                </optgroup>
+              </ng-template>
+            </ng-container>
+          </ng-template>
         </ng-container>
       </select>
     </ng-template>
