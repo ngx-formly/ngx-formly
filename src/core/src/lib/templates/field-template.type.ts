0642497502ee7ab28ae4b8f0a17c460d5b34780b
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-02-11 21:41:44 +0100

    feat(core): add safeHtml opt to bypass sanitizing template HTML… (#2090)
    fix #2039

--- src/core/src/lib/templates/field-template.type.ts
+++ src/core/src/lib/templates/field-template.type.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { FieldType } from './field.type';
 
 @Component({
   selector: 'formly-template',
-  template: `<div [innerHtml]="field.template"></div>`,
+  template: `<div [innerHtml]="template"></div>`,
 })
-export class FormlyTemplateType extends FieldType {}
+export class FormlyTemplateType extends FieldType {
+  get template() {
+    if (this.field && (this.field.template !== this.innerHtml.template)) {
+      this.innerHtml = {
+        template: this.field.template,
+        content: this.to.safeHtml
+          ? this.sanitizer.bypassSecurityTrustHtml(this.field.template)
+          : this.field.template,
+      };
+    }
+
+    return this.innerHtml.content;
+  }
+
+  private innerHtml = { content: null, template: null };
+  constructor(private sanitizer: DomSanitizer) {
+    super();
+  }
+}
