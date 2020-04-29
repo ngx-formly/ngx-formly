7a3247d0ee1034d03cc36b5ef2327e62a916fcaf
mburger81 <michi.burger@gmail.com>
2020-04-29 05:51:55 +0200

    fix(ionic): allow async options for ion-select (#2202)

--- src/ionic/src/lib/types/select.ts
+++ src/ionic/src/lib/types/select.ts
@@ -4,17 +4,20 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-field-ion-select',
   template: `
-    <ion-select
-      [formControl]="formControl"
-      [ionFormlyAttributes]="field"
-      [multiple]="to.multiple"
-      [interface]="to.interface"
-      [okText]="to.okText"
-      [cancelText]="to.cancelText">
-      <ion-select-option *ngFor="let option of to.options | formlySelectOptions:field | async" [value]="option.value">
-        {{ option.label }}
-      </ion-select-option>
-    </ion-select>
+    <!-- ng-container used as a workaround for https://github.com/ionic-team/ionic/issues/19324 -->
+    <ng-container *ngIf="to.options | formlySelectOptions:field | async; let selectOptions">
+      <ion-select
+        [formControl]="formControl"
+        [ionFormlyAttributes]="field"
+        [multiple]="to.multiple"
+        [interface]="to.interface"
+        [okText]="to.okText"
+        [cancelText]="to.cancelText">
+        <ion-select-option *ngFor="let option of selectOptions" [value]="option.value">
+            {{ option.label }}
+        </ion-select-option>
+      </ion-select>
+    </ng-container>
   `,
   styles: [':host { display: inherit; }'],
 })
