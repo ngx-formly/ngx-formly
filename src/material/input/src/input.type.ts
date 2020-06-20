cd53629783c44dd13cc296f4e3ba3617985ccad5
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-06-20 12:20:07 +0100

    fix(material): use false for input static query (#2319)
    fix #2314

--- src/material/input/src/input.type.ts
+++ src/material/input/src/input.type.ts
@@ -31,7 +31,7 @@ import { FieldType } from '@ngx-formly/material/form-field';
   `,
 })
 export class FormlyFieldInput extends FieldType implements OnInit {
-  @ViewChild(MatInput, <any> { static: true }) formFieldControl!: MatInput;
+  @ViewChild(MatInput, <any> { static: false }) formFieldControl!: MatInput;
 
   get type() {
     return this.to.type || 'text';
