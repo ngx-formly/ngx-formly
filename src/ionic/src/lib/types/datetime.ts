8cb5de8c887d14dc06c14596fb762ac0ecb2e560
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-06-28 17:37:48 +0100

    fix(ionic): add missing Names && ShortNames inputs for datetime picker
    fix #2331

--- src/ionic/src/lib/types/datetime.ts
+++ src/ionic/src/lib/types/datetime.ts
@@ -5,17 +5,22 @@ import { FieldType } from '@ngx-formly/core';
   selector: 'formly-field-ion-datetime',
   template: `
     <ion-datetime
-      [displayFormat]="to.displayFormat"
-      [pickerFormat]="to.pickerFormat"
-      [doneText]="to.doneText"
       [cancelText]="to.cancelText"
-      [min]="to.min"
-      [max]="to.max"
-      [yearValues]="to.yearValues"
-      [monthValues]="to.monthValues"
+      [dayNames]="to.dayNames"
+      [dayShortNames]="to.dayShortNames"
       [dayValues]="to.dayValues"
+      [displayFormat]="to.displayFormat"
+      [doneText]="to.doneText"
       [hourValues]="to.hourValues"
       [minuteValues]="to.minuteValues"
+      [monthNames]="to.monthNames"
+      [monthShortNames]="to.monthShortNames"
+      [monthValues]="to.monthValues"
+      [pickerFormat]="to.pickerFormat"
+      [pickerOptions]="to.pickerOptions"
+      [yearValues]="to.yearValues"
+      [min]="to.min"
+      [max]="to.max"
       [formControl]="formControl"
       [ionFormlyAttributes]="field">
     </ion-datetime>
