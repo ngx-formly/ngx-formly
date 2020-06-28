0beaaefda380a2fe5214d43d05a5d06a624114d0
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-06-28 13:07:33 +0100

    fix(core): detect changes of formState and on rebuild (#2335)

--- src/core/src/lib/utils.ts
+++ src/core/src/lib/utils.ts
@@ -274,7 +274,9 @@ export function observe<T = any>(o: IObserveTarget<T>, paths: string[], setFn: I
     state.onChange.push(setFn);
     setFn({ currentValue: state.value, firstChange: true });
     if (state.onChange.length === 1) {
+      const { enumerable } = Object.getOwnPropertyDescriptor(target, key) || { enumerable: true };
       Object.defineProperty(target, key, {
+        enumerable,
         configurable: true,
         get: () => state.value,
         set: (currentValue) => {
