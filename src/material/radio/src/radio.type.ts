0de002a96e864b4da637e35da82dc482f7e06428
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-07-11 20:27:50 +0100

    fix(material): rely on checkbox change input instead of formly attr
    fix #2353

--- src/material/radio/src/radio.type.ts
+++ src/material/radio/src/radio.type.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { FieldType } from '@ngx-formly/material/form-field';
-import { MatRadioGroup } from '@angular/material/radio';
+import { MatRadioGroup, MatRadioChange } from '@angular/material/radio';
 import { ɵwrapProperty as wrapProperty } from '@ngx-formly/core';
 
 @Component({
@@ -10,7 +10,8 @@ import { ɵwrapProperty as wrapProperty } from '@ngx-formly/core';
       [formControl]="formControl"
       [formlyAttributes]="field"
       [required]="to.required"
-      [tabindex]="to.tabindex">
+      [tabindex]="to.tabindex"
+      (change)="change($event)">
       <mat-radio-button *ngFor="let option of to.options | formlySelectOptions:field | async; let i = index;"
         [id]="id + '_' + i"
         [color]="to.color"
@@ -51,5 +52,11 @@ export class FormlyFieldRadio extends FieldType implements AfterViewInit, OnDest
 
   ngOnDestroy() {
     this.focusObserver && this.focusObserver();
+  }
+
+  change($event: MatRadioChange) {
+    if (this.to.change) {
+      this.to.change(this.field, $event);
+    }
   }
 }
