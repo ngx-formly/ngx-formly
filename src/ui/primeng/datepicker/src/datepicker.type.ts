1baaaac80016bbed8f69fadd93635faa194f9ab2
Valdir Stiebe Junior <valdir@dype.com.br>
2020-12-17 00:39:36 -0300

    fix(primeng): missing test for calendar

--- src/ui/primeng/datepicker/src/datepicker.type.ts
+++ src/ui/primeng/datepicker/src/datepicker.type.ts
@@ -4,25 +4,26 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-field-primeng-datepicker',
   template: `
-  <p-calendar 
-  [dateFormat]="to.dateFormat"
-  [hourFormat]="to.hourFormat"
-  [showTime]="to.showTime"
-  [showIcon]="to.showIcon"
-  [showButtonBar]="to.showButtonBar"
-  [showOtherMonths]="to.showOtherMonths"
-  [selectOtherMonths]="to.selectOtherMonths"
-  [selectionMode]="to.selectionMode || 'single'"
-  [numberOfMonths]="to.numberOfMonths"
-  [inline]="to.inline"
-  [readonlyInput]="to.readonlyInput"
-  [touchUI]="to.touchUI"
-  [monthNavigator]="to.monthNavigator"
-  [yearNavigator]="to.yearNavigator"
-  [yearRange]="to.yearRange"
-  [placeholder]="to.placeholder"
-  [formControl]="formControl"
-  [formlyAttributes]="field">
+  <p-calendar
+      [defaultDate]="to.defaultDate"
+      [dateFormat]="to.dateFormat"
+      [hourFormat]="to.hourFormat"
+      [showTime]="to.showTime"
+      [showIcon]="to.showIcon"
+      [showButtonBar]="to.showButtonBar"
+      [showOtherMonths]="to.showOtherMonths"
+      [selectOtherMonths]="to.selectOtherMonths"
+      [selectionMode]="to.selectionMode || 'single'"
+      [numberOfMonths]="to.numberOfMonths"
+      [inline]="to.inline"
+      [readonlyInput]="to.readonlyInput"
+      [touchUI]="to.touchUI"
+      [monthNavigator]="to.monthNavigator"
+      [yearNavigator]="to.yearNavigator"
+      [yearRange]="to.yearRange"
+      [placeholder]="to.placeholder"
+      [formControl]="formControl"
+      [formlyAttributes]="field">
   </p-calendar>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
