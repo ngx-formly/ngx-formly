e60eec3dbdabc814bce29effef5718db100dee88
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2021-01-02 22:46:04 +0100

    feat(material): mat-hint alignment support (#2646)
    Co-authored-by: Glenn Greibesland <glenn@glenng.no>
    fix #2494

---
+++ demo/src/app/examples/other/material-formfield-hint-align/config.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [
+            {
+              title: 'Material Field Hint Alignment',
+              description: `
+              <p>This demonstrates alignment of hints for Material FormFields.</p>
+
+              <p>The hintStart and hintEnd properties supports both string values and TemplateRefs.<br />
+              To use a TemplateRef, you can either supply it like shown in this example, or you can create an addon that creates the template for you like shown in the "Material prefix suffix" example</p>
+            `,
+              component: AppComponent,
+              files: [
+                {
+                  file: 'app.component.html',
+                  content: require('!!highlight-loader?raw=true&lang=html!./app.component.html'),
+                  filecontent: require('!!raw-loader!./app.component.html'),
+                },
+                {
+                  file: 'app.component.ts',
+                  content: require('!!highlight-loader?raw=true&lang=typescript!./app.component.ts'),
+                  filecontent: require('!!raw-loader!./app.component.ts'),
+                },
+                {
+                  file: 'app.module.ts',
+                  content: require('!!highlight-loader?raw=true&lang=typescript!./app.module.ts'),
+                  filecontent: require('!!raw-loader!./app.module.ts'),
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ]),
+  ],
+  entryComponents: [AppComponent],
+})
+export class ConfigModule {}
