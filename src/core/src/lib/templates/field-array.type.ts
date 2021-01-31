1c0f537d6400a0c9661b859930a25d89cb9c37c0
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2021-01-31 08:39:11 +0100

    fix(core): make FieldArray compatible with Angular 11 (#2685)
    fix #2674

--- src/core/src/lib/templates/field-array.type.ts
+++ src/core/src/lib/templates/field-array.type.ts
@@ -6,7 +6,11 @@ import { FormlyFormBuilder } from '../services/formly.form.builder';
 import { FormlyFieldConfig } from '../components/formly.field.config';
 import { FORMLY_CONFIG, FormlyExtension } from '../services/formly.config';
 import { registerControl, unregisterControl, findControl } from '../extensions/field-form/utils';
+import { Directive } from '@angular/core';
 
+// TODO remove `selector` in V6
+// tslint:disable-next-line
+@Directive({ selector: '[ɵfieldArray]' })
 export abstract class FieldArrayType<F extends FormlyFieldConfig = FormlyFieldConfig> extends FieldType<F> implements FormlyExtension {
   get formControl() {
     return this.field.formControl as FormArray;
