262df4dbaab6c3049db789557d0d8d20d3df871d
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2021-03-07 15:18:00 +0100

    feat(material): simplify custom field type

--- src/core/src/lib/templates/field.wrapper.ts
+++ src/core/src/lib/templates/field.wrapper.ts
@@ -5,4 +5,7 @@ import { FormlyFieldConfig } from '../models';
 @Directive()
 export abstract class FieldWrapper<F extends FormlyFieldConfig = FormlyFieldConfig> extends FieldType<F> {
   @ViewChild('fieldComponent', { read: ViewContainerRef }) fieldComponent: ViewContainerRef;
+  @ViewChild('fieldComponent', { read: ViewContainerRef, static: true }) set _staticContent(content: ViewContainerRef) {
+    this.fieldComponent = content;
+  }
 }
