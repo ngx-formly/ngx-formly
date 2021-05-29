1d4ff033e454086c14f815fb26e72c8b26f5cb05
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2021-05-29 20:58:15 +0100

    feat(material): add typeaheadDebounceInterval input for select
    fix #2649

--- src/ui/material/select/src/select.type.ts
+++ src/ui/material/select/src/select.type.ts
@@ -26,6 +26,7 @@ import { FieldType } from '@ngx-formly/material/form-field';
       [errorStateMatcher]="errorStateMatcher"
       [aria-labelledby]="_getAriaLabelledby()"
       [disableOptionCentering]="to.disableOptionCentering"
+      [typeaheadDebounceInterval]="to.typeaheadDebounceInterval"
     >
       <ng-container *ngIf="to.options | formlySelectOptions: field | async as selectOptions">
         <ng-container
