0f1ae020ba75af4de6aaf04354d1aa78e57867f2
MaxKless <34165455+MaxKless@users.noreply.github.com>
2021-08-09 12:49:03 +0200

    docs: add example for extending field types (#2936)

---
+++ demo/src/app/examples/advanced/extending-field-types/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      types: [
+        {
+          name: 'password',
+          extends: 'input',
+          defaultOptions: {
+            templateOptions: {
+              type: 'password',
+              label: 'Default Password Field',
+            },
+          },
+        },
+      ],
+    }),
+  ],
+  declarations: [AppComponent],
+})
+export class AppModule {}
