622592eb0668005d63ea210a22a9de23f5f1baba
MaxKless <34165455+MaxKless@users.noreply.github.com>
2022-02-25 20:16:48 +0100

    build: update build script for Windows (#3205)

--- build/util.ts
+++ build/util.ts
@@ -1,4 +1,6 @@
 import { execSync, ExecSyncOptions } from 'child_process';
+import * as fs from 'fs';
+import { join } from 'path';
 
 export const PACKAGES = [
   'core',
@@ -16,4 +18,16 @@ export const PACKAGES = [
 
 export function exec(cmd: string, options: ExecSyncOptions = { stdio: 'inherit' }) {
   return execSync(cmd, options);
+}
+
+export function copyDirSync(src, dest) {
+  fs.mkdirSync(dest, { recursive: true });
+  let entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let srcPath = join(src, entry.name);
+    let destPath = join(dest, entry.name);
+
+    entry.isDirectory() ? copyDirSync(srcPath, destPath) : fs.copyFileSync(srcPath, destPath);
+  }
 }
