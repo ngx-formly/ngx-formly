de6f5027d192a98da76abb26d4101cc229c8512b
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2022-03-13 13:11:35 +0100

    feat(core): change Formly internal extensions priority to <=-1

--- src/core/src/lib/core.module.ts
+++ src/core/src/lib/core.module.ts
@@ -23,10 +23,10 @@ export function defaultFormlyConfig(config: FormlyConfig): ConfigOption {
       { name: 'formly-template', component: FormlyTemplateType },
     ],
     extensions: [
-      { name: 'core', extension: new CoreExtension(config), priority: -1 },
-      { name: 'field-validation', extension: new FieldValidationExtension(config), priority: -1 },
-      { name: 'field-form', extension: new FieldFormExtension(), priority: -1 },
-      { name: 'field-expression', extension: new FieldExpressionExtension(), priority: -1 },
+      { name: 'core', extension: new CoreExtension(config), priority: -250 },
+      { name: 'field-validation', extension: new FieldValidationExtension(config), priority: -200 },
+      { name: 'field-form', extension: new FieldFormExtension(), priority: -150 },
+      { name: 'field-expression', extension: new FieldExpressionExtension(), priority: -100 },
     ],
   };
 }
