f2e6ce3232c58627a15b9e852b4cd0efd440063e
max.kless@googlemail.com <max.kless@googlemail.com>
2022-05-21 16:04:54 +0200

    docs(primeng): remove broken multiselect example
    3293

---
+++ demo/src/app/ui/ui-primeng/select/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+
+@Component({
+  selector: 'formly-app-example',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'Select',
+      type: 'select',
+      props: {
+        label: 'Select',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        options: [
+          { value: 1, label: 'Option 1' },
+          { value: 2, label: 'Option 2' },
+          { value: 3, label: 'Option 3' },
+          { value: 4, label: 'Option 4', disabled: true },
+        ],
+      },
+    },
+  ];
+}
