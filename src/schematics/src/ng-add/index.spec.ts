0b21a73f04e905fe028c337a04f1314057839543
Jack Woodward <jack@jcwservices.co.uk>
2022-11-22 13:31:41 +0000

    fix: fixed schematic tests

--- src/schematics/src/ng-add/index.spec.ts
+++ src/schematics/src/ng-add/index.spec.ts
@@ -1,6 +1,6 @@
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import { join } from 'path';
-import { getFileContent } from '@schematics/angular/utility/test';
+import { getFileContent } from '@schematics/angular/utility/test/get-file-content';
 import { createWorkspace, getTestProjectPath } from '../../utils/testing';
 import { Schema } from './schema';
 
@@ -21,7 +21,7 @@ describe('ng-add-schematic', () => {
       .runSchematicAsync('ng-add', {}, appTree)
       .toPromise();
     const packageJson = JSON.parse(getFileContent(tree, '/package.json'));
-
+    
     expect(packageJson.dependencies['@angular/forms']).toBeDefined();
     expect(packageJson.dependencies['@ngx-formly/core']).toBeDefined();
   });
