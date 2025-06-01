a176acabf4a2faa4361cd4f2a218ca8b5125d8f8
Abdellatif Ait Boudad <a.aitboudad@gmail.com>
2025-06-01 13:13:58 +0100

    refactor: revert redundant code in formly-field-ion-input

--- src/ui/ionic/input/src/input.type.spec.ts
+++ src/ui/ionic/input/src/input.type.spec.ts
@@ -38,7 +38,7 @@ describe('ui-ionic: Input Type', () => {
   });
 
   it('should render input[number] type', () => {
-    const { query } = renderComponent({
+    const { field, query, detectChanges } = renderComponent({
       key: 'name',
       type: 'input',
       props: { type: 'number' },
@@ -49,6 +49,12 @@ describe('ui-ionic: Input Type', () => {
       id: 'formly_1_input_name_0',
       type: 'number',
     });
+
+    const inputEl = query<HTMLInputElement>('ion-input').nativeElement;
+    inputEl.value = '1';
+    query('ion-input').triggerEventHandler('ionInput', ɵCustomEvent(inputEl));
+    detectChanges();
+    expect(field.formControl.value).toEqual(1);
   });
 
   it('should render number type', () => {
